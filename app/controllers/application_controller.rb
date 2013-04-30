ba09654a0b034a6df81fed76ce873494fb898de9
Pablo Baltodano <palinair@hotmail.com>
2013-04-30 17:34:14 +0000

    -Adding more Teams tests -Including SentientController in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   include DatatablesHelper
+  include SentientController
 
   before_filter :authenticate_user!
 
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -9,7 +9,6 @@
 #  updated_by_id :integer
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
-#  users_count   :integer          default(0)
 #  active        :boolean          default(TRUE)
 #
 
@@ -20,4 +19,25 @@ describe Team do
 
   it { should allow_mass_assignment_of(:name) }
   it { should allow_mass_assignment_of(:description) }
+
+  describe "#activate" do
+    let(:team) { FactoryGirl.build(:team, active: false) }
+
+    it "should return the active value as true" do
+      team.activate
+      team.reload
+      team.active.should be_true
+    end
+  end
+
+  describe "#deactivate" do
+    let(:team) { FactoryGirl.build(:team, active: false) }
+
+    it "should return the active value as false" do
+      team.deactivate
+      team.reload
+      team.active.should be_false
+    end
+  end
+
 end
