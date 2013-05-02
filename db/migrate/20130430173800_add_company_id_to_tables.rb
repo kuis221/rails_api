3b3dd71175a0cbec427bc969580b68fcb6f6282b
Guillermo Vargas <guilleva@gmail.com>
2013-05-02 08:58:53 -0600

    fixing migration

--- db/migrate/20130430173800_add_company_id_to_tables.rb
+++ db/migrate/20130430173800_add_company_id_to_tables.rb
@@ -4,9 +4,9 @@ class AddCompanyIdToTables < ActiveRecord::Migration
     add_column :teams, :company_id, :integer
     add_column :campaigns, :company_id, :integer
 
-    create_index :users, :company_id
-    create_index :teams, :company_id
-    create_index :campaigns, :company_id
+    add_index :users, :company_id
+    add_index :teams, :company_id
+    add_index :campaigns, :company_id
 
     c = Company.create(name: 'Test Company')
     User.update_all(:company_id => c.id)
