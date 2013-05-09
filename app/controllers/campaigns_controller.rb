19dbaed10515d0d03300921289695cd5d33382cc
Guillermo Vargas <guilleva@gmail.com>
2013-05-09 15:56:31 -0600

    Adding description column to campaigns table and renamed the column "Campaign" to "Name"

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -8,6 +8,7 @@ class CampaignsController < InheritedResources::Base
   respond_to_datatables do
     columns [
       {:attr => :name, :column_name => 'campaigns.name', :value => Proc.new{|campaign| @controller.view_context.link_to(campaign.name, @controller.view_context.campaign_path(campaign))}, :searchable => true},
+      {:attr => :description, :column_name => 'campaigns.description', :searchable => true},
       {:attr => :first_event, :value => Proc.new{|campaign| campaign.first_event.try(:start_date)}},
       {:attr => :last_event, :value => Proc.new{|campaign| campaign.last_event.try(:start_date)}},
       {:attr => :aasm_state, :column_name => 'campaigns.aasm_state', :value => Proc.new{|campaign| campaign.aasm_state.capitalize}}
