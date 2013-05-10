4689ec780dcafa33216e7e0028ce59419f962ea6
Guillermo Vargas <guilleva@gmail.com>
2013-05-10 07:58:19 -0600

    Fixing migration... again

--- db/migrate/20130509220322_rename_user_groups_table.rb
+++ db/migrate/20130509220322_rename_user_groups_table.rb
@@ -3,7 +3,7 @@ class RenameUserGroupsTable < ActiveRecord::Migration
     rename_table :user_groups, :roles
     add_column :roles, :company_id, :integer
     remove_index :users, :user_group_id
-    rename_column :users, :role_id, :role_id
+    rename_column :users, :user_group_id, :role_id
     add_index :users, :role_id
     Role.update_all(company_id: Company.first)
   end
