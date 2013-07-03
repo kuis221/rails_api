144589902f648466b165ee34112694e8299aef9f
Guillermo Vargas <guilleva@gmail.com>
2013-07-03 17:23:00 -0600

    Adding test user used for tests to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,7 @@ c = Company.find_or_create_by_name(name: 'Brandscopic')
 r = c.roles.find_or_create_by_name(name: 'Admin')
 u =  User.create({email: 'admin@brandscopic.com', first_name: 'Admin', last_name: 'Brandscopic', password: 'Adminpass12', password_confirmation: 'Adminpass12', country: 'US', state: 'CA', city: 'San Francisco', invitation_accepted_at: Time.now, confirmed_at: Time.now, invitation_token: nil}, without_protection: true)
 CompanyUser.create({active: true, user_id: u.id, company_id: c.id, role_id: r.id}, without_protection: true)
+
+# Create the user used for the load tests
+tu =  User.create({email: 'test@brandscopic.com', first_name: 'Test', last_name: 'User', password: 'TestPass321', password_confirmation: 'TestPass321', country: 'US', state: 'CA', city: 'San Francisco', invitation_accepted_at: Time.now, confirmed_at: Time.now, invitation_token: nil}, without_protection: true)
+CompanyUser.create({active: true, user_id: tu.id, company_id: c.id, role_id: r.id}, without_protection: true)
