132c0c618abaaf1b826e465701e53b293a979e9c
Guillermo Vargas <guilleva@gmail.com>
2013-08-06 14:27:14 -0600

    Making the jobs worker to use Sunspot::Queue::DelayedJob::Backend

--- config/initializers/sunspot.rb
+++ config/initializers/sunspot.rb
@@ -1,4 +1,4 @@
-unless $rails_rake_task
+unless defined?($rails_rake_task) && $rails_rake_task && !Rake.application.top_level_tasks.include?('jobs:work')
   require "sunspot/queue/delayed_job"
   backend = Sunspot::Queue::DelayedJob::Backend.new
   Sunspot.session = Sunspot::Queue::SessionProxy.new(Sunspot.session, backend)
