dea0905383aac7a10cca5cd69fde752f415b30ff
Guillermo Vargas <guilleva@gmail.com>
2013-08-23 00:44:05 -0600

    Trying to fix the memory problem with the migration script and adding more progress info

--- lib/legacy.rb
+++ lib/legacy.rb
@@ -25,15 +25,21 @@ class Legacy::Migration
     User.current = company.company_users.order('id asc').first.user
     program_ids.each do |program_id|
       program = Legacy::Program.find(program_id)
-      p "Importing #{pluralize(program.events.count, 'event')} from #{program.name}"
+      counter = 1
+      total = program.events.count
+      p "Importing #{pluralize(total, 'event')} from #{program.name}"
       campaign = program.sincronize(company).local
-      Legacy::Event.where(program_id: program).find_in_batches do |group|
+      Legacy::Event.where(program_id: program).find_in_batches(batch_size: 300) do |group|
         group.each do |legacy_event|
           migration = legacy_event.sincronize(company, {campaign_id: campaign.id})
           p migration.local.errors.inspect if migration.local.errors.any?
-          p "LevacyEvent[#{legacy_event.id}] => Event[#{migration.local.id}]"
+          p "#{counter} of #{total}: LevacyEvent[#{legacy_event.id}] => Event[#{migration.local.id}]"
+          counter+=1
         end
       end
     end
+  end
+  def self.api_client
+    @client ||= GooglePlaces::Client.new(GOOGLE_API_KEY)
   end
 end
