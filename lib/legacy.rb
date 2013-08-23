1498ec18e3da839810354bbec5cf3c4dae543e8a
Guillermo Vargas <guilleva@gmail.com>
2013-08-22 23:52:45 -0600

    Adding Legacy::Migration class to allow queing migration jobs

--- lib/legacy.rb
+++ lib/legacy.rb
@@ -15,3 +15,25 @@ require 'legacy/metric'
 Dir[Rails.root.to_s + "/lib/legacy/**/*.rb"].each do |file|
   require file
 end
+
+
+class Legacy::Migration
+  def self.company
+    @company ||= Company.find_by_name('Legacy Marketing Partners')
+  end
+  def self.synchronize_programs(program_ids)
+    User.current = company.company_users.order('id asc').first.user
+    program_ids.each do |program_id|
+      program = Legacy::Program.find(program_id)
+      p "Importing #{pluralize(program.events.count, 'event')} from #{program.name}"
+      campaign = program.sincronize(company).local
+      Legacy::Event.where(program_id: program).find_in_batches do |group|
+        group.each do |legacy_event|
+          migration = legacy_event.sincronize(company, {campaign_id: campaign.id})
+          p migration.local.errors.inspect if migration.local.errors.any?
+          p "LevacyEvent[#{legacy_event.id}] => Event[#{migration.local.id}]"
+        end
+      end
+    end
+  end
+end
