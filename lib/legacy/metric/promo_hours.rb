f1b1fe2901366f00dfc52b46c650e92f39ec7e1e
Guillermo Vargas <guilleva@gmail.com>
2013-08-24 13:38:10 -0600

    Fix for require

--- lib/legacy/metric/promo_hours.rb
+++ lib/legacy/metric/promo_hours.rb
@@ -19,6 +19,8 @@
 # INTERNAL USE - stores data in EventRecap
 # positive decimal number representing promo hours.
 # autopopulates with 1
+require 'legacy/metric/decimal'
+
 class Metric::PromoHours < Metric::Decimal
   validates_presence_of :program_id, :message => "must be a program metric"
   validates_uniqueness_of :type, :scope => :program_id
@@ -30,7 +32,7 @@ class Metric::PromoHours < Metric::Decimal
     super
     result.errors.add(:value, 'must be positive') if result.errors.empty? && cast_value(result.value) < 0
   end
-  
+
   def store_result(value, result)
     super # call super to ALSO store the data in the result - TODO does this make sense?
     result.event_recap.update_attribute(:promo_hours, cast_value(value)) if result.event_recap
