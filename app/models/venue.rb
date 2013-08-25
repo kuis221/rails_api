1f4755b1329d91483fe3ae7f8387298fb4cf2189
Guillermo Vargas <guilleva@gmail.com>
2013-08-25 16:06:10 -0600

    Fixes on venue details

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -169,24 +169,25 @@ class Venue < ActiveRecord::Base
 
     # First let the DB to do the math for the events that starts and ends the same day... (the easy part)
     tz = Time.zone.now.strftime('%Z')
-    stats_by_day = Event.select("count(events.id) AS counting, avg(events.promo_hours) as promo_hours, EXTRACT(DOW FROM events.start_at AT TIME ZONE '#{tz}') AS weekday")
+    stats_by_day = Event.select("count(events.id) AS counting, sum(events.promo_hours) as promo_hours, sum(event_data.impressions) as impressions, EXTRACT(DOW FROM events.start_at AT TIME ZONE '#{tz}') AS weekday")
+         .joins(:event_data)
          .group("EXTRACT(DOW FROM events.start_at AT TIME ZONE '#{tz}')")
          .where(place_id: place_id, company_id: company_id)
          .where(["date_trunc('day',start_at AT TIME ZONE ?) = date_trunc('day',end_at AT TIME ZONE ?)", tz, tz])
     @overall_graphs_data[:trends_week_day] = Hash[(0..6).map{|i|[i, 0]}]
-    stats_by_day.each{|s| @overall_graphs_data[:trends_week_day][(s.weekday == '0' ? 6 : s.weekday.to_i-1)] = s.promo_hours.to_f }
+    stats_by_day.each{|s| @overall_graphs_data[:trends_week_day][(s.weekday == '0' ? 6 : s.weekday.to_i-1)] = s.impressions.to_f / s.promo_hours.to_f }
 
     # Then we handle the case when the events ends on a different day manually because coudn't think on a better way to do it
-    events = Event.where(place_id: place_id, company_id: company_id)
+    events = Event.select('events.*, event_data.impressions').where(place_id: place_id, company_id: company_id)
+         .joins(:event_data)
          .where(["date_trunc('day',start_at AT TIME ZONE ?) <> date_trunc('day',end_at AT TIME ZONE ?)", tz, tz])
+         .where('events.promo_hours > 0')
     events.each do |e|
       (e.start_at.to_date..e.end_at.to_date).each do |day|
         hours = ([e.end_at, day.end_of_day].min - [e.start_at, day.beginning_of_day].max) / 3600
-        @overall_graphs_data[:trends_week_day][(day.wday == 0 ? 6 : day.wday-1)] += hours
+        @overall_graphs_data[:trends_week_day][(day.wday == 0 ? 6 : day.wday-1)] += (e.impressions.to_i/e.promo_hours * hours)
       end
     end
-
-
 
     @overall_graphs_data
   end
