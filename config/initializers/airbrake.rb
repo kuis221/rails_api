05ad5f624bee0baaa2766abd8eccf6d8153ed99e
Guillermo Vargas <guilleva@gmail.com>
2013-09-19 08:46:09 -0600

    Using the same app for both environments

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,12 +1,4 @@
-if Rails.env.production?
-  Airbrake.configure do |config|
-    config.api_key = '4c06476135f92dd6dd572613f9a68434'
-    config.host        = 'errors.jaskotmedia.com'
-    config.port        = 80
-    config.secure      = config.port == 443
-    config.development_environments = ['development', 'test']
-  end
-elsif Rails.env.staging?
+if Rails.env.production? or  Rails.env.staging?
   Airbrake.configure do |config|
     config.api_key = 'ab711b3a909036274cbd1ea24809ec9f'
     config.host        = 'errors.jaskotmedia.com'
