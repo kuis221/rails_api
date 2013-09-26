787190ca297abde3b3a55621c8e2f2909b35fccd
Guillermo Vargas <guilleva@gmail.com>
2013-09-26 16:33:42 -0600

    fixing bug on events export and making resque to report errors on airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,10 @@
-if Rails.env.production? or  Rails.env.staging?
+if Rails.env.production? or Rails.env.staging?
+  require 'resque/failure/redis'
+  require 'resque/failure/multiple'
+  require 'resque/failure/airbrake'
+  Resque::Failure::Multiple.classes = [Resque::Failure::Redis, Resque::Failure::Airbrake]
+  Resque::Failure.backend = Resque::Failure::Multiple
+
   Airbrake.configure do |config|
     config.api_key = 'ab711b3a909036274cbd1ea24809ec9f'
     config.host        = 'errors.jaskotmedia.com'
