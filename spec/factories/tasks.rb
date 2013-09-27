0d7a97c5ac88ceb23ed630fcc7e16725ec814258
Guillermo Vargas <guilleva@gmail.com>
2013-09-27 11:10:28 -0600

    Add TASK STATUS filter to My Tasks and Team Tasks filter

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
     event_id 1
     title "MyString"
     due_at "2013-05-02 15:56:44"
-    company_user_id 1
+    company_user_id nil
     completed false
 
     factory :late_task do
