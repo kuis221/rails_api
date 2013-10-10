ded391252c77c4b2dc90cb7760ae7f9eb9e6458f
Guillermo Vargas <guilleva@gmail.com>
2013-10-09 23:50:22 -0600

    Do not import values for disabled KPIs

--- lib/legacy/event.rb
+++ lib/legacy/event.rb
@@ -68,6 +68,7 @@ class Legacy::Event < Legacy::Record
   end
 
   def event_recap_attributes(event)
+    active_kpis = event.campaign.active_kpis
     event.aasm_state = event_recap.new_record? ? 'unsent' : event_recap.state
 
     # Event summary
@@ -84,46 +85,57 @@ class Legacy::Event < Legacy::Record
     end
 
     # Consumer Impressions
-    result = event.result_for_kpi(Kpi.impressions)
-    result.value = event_recap.result_for_metric(Metric.system.find_by_name('# Consumer Impressions')).try(:scalar_value)
-    result.value = result.value.to_s unless result.value.nil?
+    if active_kpis.include?(Kpi.impressions)
+      result = event.result_for_kpi(Kpi.impressions)
+      result.value = event_recap.result_for_metric(Metric.system.find_by_name('# Consumer Impressions')).try(:scalar_value)
+      result.value = result.value.to_s unless result.value.nil?
+    end
 
     # Consumers Sampled
-    result = event.result_for_kpi(Kpi.samples)
-    result.value = event_recap.result_for_metric(Metric.system.find_by_name('# Consumers Sampled')).try(:scalar_value)
-    result.value = result.value.to_s unless result.value.nil?
+    if active_kpis.include?(Kpi.samples)
+      result = event.result_for_kpi(Kpi.samples)
+      result.value = event_recap.result_for_metric(Metric.system.find_by_name('# Consumers Sampled')).try(:scalar_value)
+      result.value = result.value.to_s unless result.value.nil?
+    end
 
     # Consumers Interactions
-    result = event.result_for_kpi(Kpi.interactions)
-    result.value = event_recap.result_for_metric(Metric.system.find_by_name('# Consumer Interactions')).try(:scalar_value)
-    result.value = result.value.to_s unless result.value.nil?
+    if active_kpis.include?(Kpi.interactions)
+      result = event.result_for_kpi(Kpi.interactions)
+      result.value = event_recap.result_for_metric(Metric.system.find_by_name('# Consumer Interactions')).try(:scalar_value)
+      result.value = result.value.to_s unless result.value.nil?
+    end
 
     # Gender
     # [[1, "Male"], [2, "Female"]]
-    kpi_results = event.result_for_kpi(Kpi.gender)
-    values = event_recap.result_for_metric(Metric.system.find_by_name('Gender')).try(:value)
-    kpi_results.detect{|r| r.kpis_segment.text == 'Male' }.try('value=', values[1].to_s)
-    kpi_results.detect{|r| r.kpis_segment.text == 'Female' }.try('value=', values[2].to_s)
+    if active_kpis.include?(Kpi.gender)
+      kpi_results = event.result_for_kpi(Kpi.gender)
+      values = event_recap.result_for_metric(Metric.system.find_by_name('Gender')).try(:value)
+      kpi_results.detect{|r| r.kpis_segment.text == 'Male' }.try('value=', values[1].to_s)
+      kpi_results.detect{|r| r.kpis_segment.text == 'Female' }.try('value=', values[2].to_s)
+    end
 
     # Age
     # [[3, "LDA-27"], [4, "28-35"], [5, "36-44"], [6, "45-55"], [7, "56+"]]
-    kpi_results = event.result_for_kpi(Kpi.age)
-    values = event_recap.result_for_metric(Metric.system.find_by_name('Age')).try(:value)
-    kpi_results.detect{|r| r.kpis_segment.text == '18 – 24' }.try('value=', values[3].to_s)
-    kpi_results.detect{|r| r.kpis_segment.text == '25 – 34' }.try('value=', values[4].to_s)
-    kpi_results.detect{|r| r.kpis_segment.text == '35 – 44' }.try('value=', values[5].to_s)
-    kpi_results.detect{|r| r.kpis_segment.text == '45 – 54' }.try('value=', values[6].to_s)
-    kpi_results.detect{|r| r.kpis_segment.text == '55 – 64' }.try('value=', values[7].to_s)
+    if active_kpis.include?(Kpi.age)
+      kpi_results = event.result_for_kpi(Kpi.age)
+      values = event_recap.result_for_metric(Metric.system.find_by_name('Age')).try(:value)
+      kpi_results.detect{|r| r.kpis_segment.text == '18 – 24' }.try('value=', values[3].to_s)
+      kpi_results.detect{|r| r.kpis_segment.text == '25 – 34' }.try('value=', values[4].to_s)
+      kpi_results.detect{|r| r.kpis_segment.text == '35 – 44' }.try('value=', values[5].to_s)
+      kpi_results.detect{|r| r.kpis_segment.text == '45 – 54' }.try('value=', values[6].to_s)
+      kpi_results.detect{|r| r.kpis_segment.text == '55 – 64' }.try('value=', values[7].to_s)
+    end
 
     # Ethnicity
     # [[9, "African American"], [10, "Asian"], [11, "Hispanic"], [12, "Other"], [8, "General Market"]]
-    kpi_results = event.result_for_kpi(Kpi.ethnicity)
-    values = event_recap.result_for_metric(Metric.system.find_by_name('Demographic')).try(:value)
-    kpi_results.detect{|r| r.kpis_segment.text == 'Asian' }.try('value=', values[10].to_s)
-    kpi_results.detect{|r| r.kpis_segment.text == 'Black / African American' }.try('value=', values[9].to_s)
-    kpi_results.detect{|r| r.kpis_segment.text == 'Hispanic / Latino' }.try('value=', values[11].to_s)
-    kpi_results.detect{|r| r.kpis_segment.text == 'White' }.try('value=', values[8].to_s)
-
+    if active_kpis.include?(Kpi.ethnicity)
+      kpi_results = event.result_for_kpi(Kpi.ethnicity)
+      values = event_recap.result_for_metric(Metric.system.find_by_name('Demographic')).try(:value)
+      kpi_results.detect{|r| r.kpis_segment.text == 'Asian' }.try('value=', values[10].to_s)
+      kpi_results.detect{|r| r.kpis_segment.text == 'Black / African American' }.try('value=', values[9].to_s)
+      kpi_results.detect{|r| r.kpis_segment.text == 'Hispanic / Latino' }.try('value=', values[11].to_s)
+      kpi_results.detect{|r| r.kpis_segment.text == 'White' }.try('value=', values[8].to_s)
+    end
 
     # Custom KPIs
     program.form_template.form_fields.custom.each do |field|
