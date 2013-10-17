0573157e251c00c860883a0257240e190ad2ba6e
Guillermo Vargas <guilleva@gmail.com>
2013-10-17 09:09:50 -0600

    Fixing error when non admin users try to export a list

--- app/models/company_user.rb
+++ app/models/company_user.rb
@@ -146,7 +146,7 @@ class CompanyUser < ActiveRecord::Base
 
   def accessible_places
     # TODO: memcache this
-    @accessible_places ||= (user.current_company_user.place_ids + user.current_company_user.areas.map{|a| a.places.map{|p| p.id}}).flatten.uniq
+    @accessible_places ||= (place_ids + areas.map{|a| a.places.map{|p| p.id}}).flatten.uniq
   end
 
   class << self
