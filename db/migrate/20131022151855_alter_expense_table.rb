e626b61721d991eebba7e2b7dfb7d64bc4d9da19
Guillermo Vargas <guilleva@gmail.com>
2013-10-23 14:45:45 -0600

    Increasing the number of tries

--- db/migrate/20131022151855_alter_expense_table.rb
+++ db/migrate/20131022151855_alter_expense_table.rb
@@ -1,11 +1,12 @@
 class AlterExpenseTable < ActiveRecord::Migration
   def up
     EventExpense.all.each do |expense|
-      tries = 5
+      tries = 15
       begin
         AttachedAsset.create({attachable: expense, file: expense.file, asset_type: 'expense', processed: true}, without_protection: true) if expense.file.exists?
       rescue AWS::S3::Errors::RequestTimeout => e
         sleep(3)
+        p "Failed... #{tries} tries left!"
         retry unless (tries -= 1) <= 0
         raise e
       end
