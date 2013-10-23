0213e03a8fb5e8d0b547ce4afcad9e31ed899c4e
Guillermo Vargas <guilleva@gmail.com>
2013-10-23 14:35:56 -0600

    Add delay and raise a more descriptive description

--- db/migrate/20131022151855_alter_expense_table.rb
+++ db/migrate/20131022151855_alter_expense_table.rb
@@ -1,12 +1,13 @@
 class AlterExpenseTable < ActiveRecord::Migration
   def up
     EventExpense.all.each do |expense|
-      tries = 3
+      tries = 5
       begin
         AttachedAsset.create({attachable: expense, file: expense.file, asset_type: 'expense', processed: true}, without_protection: true) if expense.file.exists?
-      rescue AWS::S3::Errors::RequestTimeout
+      rescue AWS::S3::Errors::RequestTimeout => e
+        sleep(3)
         retry unless (tries -= 1) <= 0
-        raise "Cannot save attached asset for #{expense.inspect}"
+        raise e
       end
       expense.file = nil
       expense.save
