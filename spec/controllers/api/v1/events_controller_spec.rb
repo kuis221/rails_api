7a7729f47bb4115e80017838acf5420f9193f080
Pablo Baltodano <palinair@hotmail.com>
2013-10-29 10:57:02 -0600

    Adding tests for API update events

--- spec/controllers/api/v1/events_controller_spec.rb
+++ spec/controllers/api/v1/events_controller_spec.rb
@@ -57,4 +57,28 @@ describe Api::V1::EventsController do
       event.promo_hours.should == 1
     end
   end
+
+  describe "PUT 'update'" do
+    let(:event){ FactoryGirl.create(:event, company: company) }
+    it "must update the event attributes" do
+      place = FactoryGirl.create(:place)
+      put 'update', auth_token: user.authentication_token, company_id: company.to_param, id: event.to_param, event: {campaign_id: 111, start_date: '05/21/2020', start_time: '12:00pm', end_date: '05/22/2020', end_time: '01:00pm', place_id: place.id, active: 'false'}, format: :json
+      assigns(:event).should == event
+      response.should be_success
+      event.reload
+      event.campaign_id.should == 111
+      event.start_at.should == Time.zone.parse('2020-05-21 12:00:00')
+      event.end_at.should == Time.zone.parse('2020-05-22 13:00:00')
+      event.place_id.should == place.id
+      event.promo_hours.to_i.should == 25
+      event.active.should == false
+    end
+
+    it "must update the event attributes" do
+      place = FactoryGirl.create(:place)
+      put 'update', auth_token: user.authentication_token, company_id: company.to_param, id: event.to_param, partial: 'event_data', event: {campaign_id: FactoryGirl.create(:campaign, company: @company).to_param, start_date: '05/21/2020', start_time: '12:00pm', end_date: '05/22/2020', end_time: '01:00pm', place_id: place.id}, format: :json
+      assigns(:event).should == event
+      response.should be_success
+    end
+  end
 end
