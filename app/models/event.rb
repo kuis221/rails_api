5432ea041f3f68da95780a9325f8a0bca7a69da4
Guillermo Vargas <guilleva@gmail.com>
2013-11-05 10:24:30 -0600

    Reindex the venue every time the event is saved

--- app/models/event.rb
+++ app/models/event.rb
@@ -570,7 +570,7 @@ class Event < ActiveRecord::Base
         event_data.save
       end
 
-      if (@refresh_event_data || place_id_changed? || active_changed?) && place_id.present?
+      if place_id.present?
         Resque.enqueue(VenueIndexer, venue.id)
       end
 
