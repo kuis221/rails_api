f99b03a7c59fc8ecbf01581e81f9f7500f7fd7ea
Guillermo Vargas <guilleva@gmail.com>
2013-11-11 01:06:01 -0600

    mailtrap config

--- config/initializers/mailtrap.rb
+++ config/initializers/mailtrap.rb
@@ -1,12 +1,11 @@
 if ENV['MAILTRAP_HOST'].present?
-  Brandscopic::Application.configure do
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      :user_name => ENV['MAILTRAP_USER_NAME'],
-      :password => ENV['MAILTRAP_PASSWORD'],
-      :address => ENV['MAILTRAP_HOST'],
-      :port => ENV['MAILTRAP_PORT'],
-      :authentication => :plain
-    }
-  end
+  ActionMailer::Base.smtp_settings = {
+    :address        => ENV['MAILTRAP_HOST'],
+    :port           => ENV['MAILTRAP_PORT'],
+    :authentication => :plain,
+    :user_name      => ENV['MAILTRAP_USER_NAME'],
+    :password       => ENV['MAILTRAP_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
