a9d8087ce26bbdada97703b826ac99317adfadf8
Guillermo Vargas <guilleva@gmail.com>
2013-11-11 00:52:18 -0600

    Adding mailtrap for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -70,13 +70,15 @@ Brandscopic::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :user_name => 'brandscopic-681f1489f726c516',
-    :password => '6834e16ee71eb114',
-    :address => 'mailtrap.io',
-    :port => '2525',
-    :authentication => :plain
-  }
 end
+
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
