6a689128c77957eeee41cf200de28dc6ca443eeb
Pablo Baltodano <palinair@hotmail.com>
2013-11-19 12:18:40 -0600

    Story: I want to add a new venue from the venues section

--- config/routes.rb
+++ config/routes.rb
@@ -223,7 +223,6 @@ Brandscopic::Application.routes.draw do
       match 'teams/:team_id' => 'events#delete_member', via: :delete, as: :delete_team
       match 'members/new' => 'events#new_member', via: :get, as: :new_member
       match 'members' => 'events#add_members', via: :post, as: :add_member
-
     end
   end
 
@@ -264,7 +263,7 @@ Brandscopic::Application.routes.draw do
     end
   end
 
-  resources :places, only: [] do
+  resources :places, only: [:create, :new] do
     get :search, format: :json, on: :collection
     resources :areas, only: [:new, :create]
   end
