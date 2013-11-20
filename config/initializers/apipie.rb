badb931de598b224644952e1ab85842677c0ec50
Guillermo Vargas <guilleva@gmail.com>
2013-11-19 21:09:54 -0600

    Adding authentication to api documentation

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -4,4 +4,11 @@ Apipie.configure do |config|
   config.doc_base_url            = "/apipie"
   # were is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/v1/*.rb"
+
+
+  config.authenticate = Proc.new do
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "brandscopic" && password == "BSP1k2014"
+    end
+  end
 end
