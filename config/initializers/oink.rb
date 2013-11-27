5ee4c8cc394ae108e098c326358e61b6db2a179e
Guillermo Vargas <guilleva@gmail.com>
2013-11-26 18:32:14 -0600

    Making production and environment to send the oink output to default log

--- config/initializers/oink.rb
+++ config/initializers/oink.rb
@@ -1,1 +1,5 @@
-Rails.application.middleware.use Oink::Middleware
+if Rails.env.production? || Rails.env.staging?
+  Rails.application.middleware.use( Oink::Middleware, :logger => Hodel3000CompliantLogger.new(STDOUT), :instruments => :memory)
+else
+  Rails.application.middleware.use Oink::Middleware
+end
