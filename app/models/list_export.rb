ec2d4e8137f922b0f1d69b0503b3bfb7fc6ab81d
Guillermo Vargas <guilleva@gmail.com>
2013-12-17 20:51:40 -0600

    Fixing date on exports when timezone support is turned on

--- app/models/list_export.rb
+++ app/models/list_export.rb
@@ -66,6 +66,7 @@ class ListExport < ActiveRecord::Base
     controller = self.controller.constantize.new
 
     User.current = company_user.user
+    Company.current = company_user.company
     controller.instance_variable_set(:@_current_user, company_user.user)
     controller.instance_variable_set(:@current_company, company_user.company)
     controller.instance_variable_set(:@current_company_user, company_user)
