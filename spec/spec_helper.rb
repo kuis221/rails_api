11dcb50735c1b75ebad770d72b90d4b8fa86e48b
Guillermo Vargas <guilleva@gmail.com>
2013-12-18 11:20:21 -0600

    Fix on screenshot saving for CI server

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -73,18 +73,19 @@ RSpec.configure do |config|
 
   if ENV['CI']
     Capybara::Screenshot.autosave_on_failure = false
+    config.after(:each) do
+      # Save screenshot to Amazon S3 on failure when running on the CI server
+      if Capybara.page.current_url != '' && example.exception
+        filename_prefix = Capybara::Screenshot.filename_prefix_for(:rspec, example)
+        saver = Capybara::Screenshot::Saver.new(Capybara, Capybara.page, true, filename_prefix)
+        saver.save
 
-    # Save screenshot to Amazon S3 on failure when running on the CI server
-    if Capybara.page.current_url != '' && example.exception
-      filename_prefix = Capybara::Screenshot.filename_prefix_for(:rspec, example)
-      saver = Capybara::Screenshot::Saver.new(Capybara, Capybara.page, true, filename_prefix)
-      saver.save
-
-      # Save it to S3
-      s3 = AWS::S3.new
-      bucket = s3.buckets[S3_CONFIGS['bucket_name']]
-      obj = bucket.objects['key'].write(File.open(saver.screenshot_path))
-      example.metadata[:full_description] += "\n     Screenshot: #{obj.url_for(:read)}"
+        # Save it to S3
+        s3 = AWS::S3.new
+        bucket = s3.buckets[S3_CONFIGS['bucket_name']]
+        obj = bucket.objects['key'].write(File.open(saver.screenshot_path))
+        example.metadata[:full_description] += "\n     Screenshot: #{obj.url_for(:read)}"
+      end
     end
   end
 
