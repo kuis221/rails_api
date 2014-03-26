a3c53dff4cb67fd58c403d53a7b7a95b794c7caa
Guillermo Vargas <guilleva@gmail.com>
2014-03-26 10:33:17 -0600

    Adding assets precompilation

--- config/application.rb
+++ config/application.rb
@@ -62,7 +62,12 @@ module Brandscopic
 
     config.assets.initialize_on_precompile = false
 
-    config.assets.precompile += %w[admin/active_admin.css admin/active_admin.js jquery.placesAutocomplete.js]
+    config.assets.precompile += %w[
+      admin/active_admin.css admin/active_admin.js
+      jquery.placesAutocomplete.js
+      reports.css form_builder.css
+      jquery.campaignFormBuilder.js
+      ]
 
     config.cache_store = :dalli_store
 
