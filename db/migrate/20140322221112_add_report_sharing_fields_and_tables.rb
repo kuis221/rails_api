d6c288ef6a6702aa2f912f7c644130a7a24277e8
Guillermo Vargas <guilleva@gmail.com>
2014-03-27 16:21:10 -0600

    Check to avoid error when running the migration again with different name

--- db/migrate/20140322221112_add_report_sharing_fields_and_tables.rb
+++ db/migrate/20140322221112_add_report_sharing_fields_and_tables.rb
@@ -1,12 +1,14 @@
 class AddReportSharingFieldsAndTables < ActiveRecord::Migration
   def change
-    create_table :report_sharings do |t|
-      t.references :report
-      t.integer :shared_with_id
-      t.string :shared_with_type
-    end
-    add_index :report_sharings, [:shared_with_id, :shared_with_type]
+  	unless ActiveRecord::Base.connection.table_exists? 'report_sharings'
+      create_table :report_sharings do |t|
+        t.references :report
+        t.integer :shared_with_id
+        t.string :shared_with_type
+      end
+      add_index :report_sharings, [:shared_with_id, :shared_with_type]
 
-    add_column :reports, :sharing, :string, default: 'owner'
+      add_column :reports, :sharing, :string, default: 'owner'
+    end
   end
 end
