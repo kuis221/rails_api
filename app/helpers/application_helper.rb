138c7f3df524d84314e4c100f0de715c1ed258b1
Guillermo Vargas <guilleva@gmail.com>
2014-04-10 15:42:24 -0600

    Fixing affix bar position when new-feature-alert is visible

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -53,7 +53,7 @@ module ApplicationHelper
   end
 
   def resource_details_bar(title, url = '')
-    content_tag(:div, id: 'resource-close-details', 'data-spy' => "affix", 'data-offset-top' => "20") do
+    content_tag(:div, id: 'resource-close-details', 'data-spy' => "affix", 'data-offset-top' => "200") do
       link_to(resource_close_bar_url, class: 'close-details') do
         content_tag(:span, title, class: 'details-bar-pull-left') +
         content_tag(:span, " ".html_safe, class: :close)
