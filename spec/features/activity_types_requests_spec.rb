4db5f080239331293cc75bbe90b897457abe246f
Guillermo Vargas <guilleva@gmail.com>
2014-04-15 17:47:06 -0600

    When the name of an activity type is updated from the list view, that change must be reflected without having to refresh the page

--- spec/features/activity_types_requests_spec.rb
+++ spec/features/activity_types_requests_spec.rb
@@ -55,33 +55,78 @@ feature "ActivityTypes", js: true do
     end
 
     scenario "should allow user to deactivate activity types" do
-        FactoryGirl.create(:activity_type, name: 'A Vinos ticos', description: 'Algunos vinos de Costa Rica', active: true, company: @company)
-        Sunspot.commit
-        visit activity_types_path
+      FactoryGirl.create(:activity_type, name: 'A Vinos ticos', description: 'Algunos vinos de Costa Rica', active: true, company: @company)
+      Sunspot.commit
+      visit activity_types_path
 
-        expect(page).to have_content('A Vinos ticos')
-        within("ul#activity_types-list li:nth-child(1)") do
-          click_js_link('Deactivate')
-        end
-        confirm_prompt 'Are you sure you want to deactivate this activity type?'
+      expect(page).to have_content('A Vinos ticos')
+      within("ul#activity_types-list li:nth-child(1)") do
+        click_js_link('Deactivate')
+      end
+      confirm_prompt 'Are you sure you want to deactivate this activity type?'
 
-        expect(page).to have_no_content('A Vinos ticos')
+      expect(page).to have_no_content('A Vinos ticos')
+    end
+
+    scenario "should allow user to activate activity type" do
+      FactoryGirl.create(:activity_type, name: 'A Vinos ticos', description: 'Algunos vinos de Costa Rica', active: false, company: @company)
+      Sunspot.commit
+      visit activity_types_path
+
+      filter_section('ACTIVE STATE').unicheck('Inactive')
+      filter_section('ACTIVE STATE').unicheck('Active')
+
+      expect(page).to have_content('A Vinos ticos')
+      within("ul#activity_types-list li:nth-child(1)") do
+        click_js_link('Activate')
+      end
+      expect(page).to have_no_content('A Vinos ticos')
+    end
+
+    scenario "should allow user to edit an activity type" do
+      FactoryGirl.create(:activity_type, name: 'A test activity type', description: 'Algunos vinos de Costa Rica', company: @company)
+      Sunspot.commit
+      visit activity_types_path
+
+      expect(page).to have_content('A test activity type')
+      within("ul#activity_types-list li:nth-child(1)") do
+        click_js_link('Edit')
       end
 
-      scenario "should allow user to activate activity type" do
-        FactoryGirl.create(:activity_type, name: 'A Vinos ticos', description: 'Algunos vinos de Costa Rica', active: false, company: @company)
-        Sunspot.commit
-        visit activity_types_path
-
-        filter_section('ACTIVE STATE').unicheck('Inactive')
-        filter_section('ACTIVE STATE').unicheck('Active')
-
-        expect(page).to have_content('A Vinos ticos')
-        within("ul#activity_types-list li:nth-child(1)") do
-          click_js_link('Activate')
-        end
-        expect(page).to have_no_content('A Vinos ticos')
+      within visible_modal do
+        fill_in 'Name', with: 'Drink feature'
+        fill_in 'Description', with: 'A description for drink feature type'
+        click_js_button 'Save'
       end
+      ensure_modal_was_closed
+
+      within("ul#activity_types-list li:nth-child(1)") do
+        expect(page).to have_no_content('A test activity type')
+        expect(page).to have_content('Drink feature')
+        expect(page).to have_content('A description for drink feature type')
+      end
+    end
+  end
+
+  feature "activity type details view" do
+    scenario "should allow user to edit an activity type" do
+      activity_type = FactoryGirl.create(:activity_type, name: 'A test activity type', description: 'Algunos vinos de Costa Rica', company: @company)
+      Sunspot.commit
+      visit activity_type_path(activity_type)
+
+      expect(page).to have_selector('h2', text: 'A test activity type')
+      find('.links-data').click_js_link('Edit')
+
+      within visible_modal do
+        fill_in 'Name', with: 'Drink feature'
+        fill_in 'Description', with: 'A description for drink feature type'
+        click_js_button 'Save'
+      end
+      ensure_modal_was_closed
+
+      expect(page).to have_selector('h2', text: 'Drink feature')
+      expect(page).to have_selector('div.description-data', text: 'A description for drink feature type')
+    end
   end
 
   feature "report builder" do
