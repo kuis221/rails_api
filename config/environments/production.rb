675c574a6353620efb0653f7cd2abf3208c99516
Guillermo Vargas <guilleva@gmail.com>
2014-07-01 23:25:20 -0600

    Adding cloudfront configuration for production/staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,7 @@ Brandscopic::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "dym0dmjiz6nk2.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -49,6 +49,8 @@ Brandscopic::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
+  config.action_controller.asset_host = "d1tlq2pjjok8jf.cloudfront.net"
+
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
