4fd8f808846c1773767ee04391f27d9853fbc34f
Guillermo Vargas <guilleva@gmail.com>
2014-07-08 13:03:26 -0600

    Fixing app start on staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Brandscopic::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
   config.action_controller.asset_host = ENV['ASSETS_HOST'] if ENV['ASSETS_HOST']
-  config.font_assets.origin = "http://#{ENV['HOST_DOMAIN']}"
+  config.font_assets.origin = "http://#{ENV['HOST_DOMAIN']}" if ENV['WEB']
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
