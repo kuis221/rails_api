5b3153288fcc087a06cee0f699e31b8a1ebf125b
Guillermo Vargas <guilleva@gmail.com>
2014-09-01 19:20:08 -0600

    Adding options to edit/deactivate visits from the visit details view

--- app/helpers/deactivable_helper.rb
+++ app/helpers/deactivable_helper.rb
@@ -3,9 +3,9 @@ module DeactivableHelper
   module ViewMethods
     def active_inactive_buttons(resource, message=nil)
       if resource.active?
-        link_to(content_tag(:i, '', class: 'icon-rounded-disable'),  url_for(params.merge(action: :deactivate, id: resource.id)), class: 'toggle-inactive active-toggle-btn-'+resource.class.name.underscore.downcase+'-'+resource.id.to_s, remote: true, title: I18n.t('confirmation.deactivate'), data: { confirm: I18n.t('confirmation.deactivate_confirm_message', model: resource.class.model_name.human.downcase)})
+        link_to(content_tag(:i, '', class: 'icon-rounded-disable'),  url_for(params.merge(action: :deactivate, id: resource.id)), class: 'toggle-inactive active-toggle-btn-'+resource.class.name.underscore.gsub('/', '_').downcase+'-'+resource.id.to_s, remote: true, title: I18n.t('confirmation.deactivate'), data: { confirm: I18n.t('confirmation.deactivate_confirm_message', model: resource.class.model_name.human.downcase)})
       else
-        link_to(content_tag(:i, '', class: 'icon-rounded-ok'), url_for(params.merge(action: :activate, id: resource.id)), class: 'toggle-active active-toggle-btn-'+resource.class.name.underscore.downcase+'-'+resource.id.to_s, remote: true, title: I18n.t('confirmation.activate'))
+        link_to(content_tag(:i, '', class: 'icon-rounded-ok'), url_for(params.merge(action: :activate, id: resource.id)), class: 'toggle-active active-toggle-btn-'+resource.class.name.underscore.gsub('/', '_').downcase+'-'+resource.id.to_s, remote: true, title: I18n.t('confirmation.activate'))
       end
     end
   end
