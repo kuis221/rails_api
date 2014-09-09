fe652d56a07ca398fc062076553a32bebcc4b565
Guillermo Vargas <guilleva@gmail.com>
2014-09-08 20:19:59 -0600

    Adding wkhtmltopdf binary for production

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,17 @@
+wkhtmltopdf_path = if Rails.env.production? || ENV['CI']
+ Rails.root.join('bin', 'wkhtmltopdf').to_s
+elsif ENV['WKHTMLTOPDF_PATH']
+  ENV['WKHTMLTOPDF_PATH']
+elsif /darwin/ =~ RUBY_PLATFORM then
+  '/usr/local/bin/wkhtmltopdf'
+elsif /linux/ =~ RUBY_PLATFORM then
+  '/usr/bin/wkhtmltopdf'
+else
+  Rails.logger.debug "\n\n **** WARNING: wkhtmltopdf NOT FOUND. PDF exports will not work ****\n\n"
+end
+
 WickedPdf.config = {
-  :wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
+  :wkhtmltopdf => wkhtmltopdf_path,
   #:layout => "pdf.html",
-  :exe_path => '/usr/local/bin/wkhtmltopdf'
+  :exe_path => wkhtmltopdf_path
 }
