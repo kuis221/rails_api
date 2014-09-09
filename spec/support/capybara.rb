81ec9a94e4f03c614c73b49e70176c9ec22d321d
Guillermo Vargas <guilleva@gmail.com>
2014-09-09 09:33:09 -0600

    Fixes on PDF styles

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,6 +2,6 @@
 #Capybara.javascript_driver = :selenium
 Capybara.javascript_driver = :poltergeist
 Capybara.default_wait_time = 5
-Capybara.server_port = 7000
-Capybara.app_host = "http://localhost:#{Capybara.server_port}"
-ActionController::Base.asset_host = Capybara.app_host
+# Capybara.server_port = 7000
+# Capybara.app_host = "http://localhost:#{Capybara.server_port}"
+# ActionController::Base.asset_host = Capybara.app_host
