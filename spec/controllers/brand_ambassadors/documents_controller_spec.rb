2084eff835e7fa258893c333af4c2ffb1af98a00
Guillermo Vargas <guilleva@gmail.com>
2014-09-17 18:42:27 -0600

    Upload documents to the current folder

--- spec/controllers/brand_ambassadors/documents_controller_spec.rb
+++ spec/controllers/brand_ambassadors/documents_controller_spec.rb
@@ -26,57 +26,69 @@ RSpec.describe BrandAmbassadors::DocumentsController, :type => :controller do
   end
 
   describe "POST 'create'" do
-    it "should successfully create the new record" do
-      ResqueSpec.reset!
-      s3object = double()
-      allow(s3object).to receive(:copy_from).and_return(true)
-      expect_any_instance_of(AWS::S3).to receive(:buckets).at_least(:once).and_return(
-        "brandscopic-dev" => double(objects: {
-          'uploads/dummy/test.jpg' => double(head: double(content_length: 100, content_type: 'image/jpeg', last_modified: Time.now)),
-          'attached_assets/original/test.jpg' => s3object
-        } ))
-      expect_any_instance_of(Paperclip::Attachment).to receive(:path).and_return('/attached_assets/original/test.jpg')
-      expect_any_instance_of(AttachedAsset).to receive(:download_url).at_least(:once).and_return('dummy.jpg')
-      expect {
-        xhr :post, 'create', brand_ambassadors_document: {direct_upload_url: 'https://s3.amazonaws.com/brandscopic-dev/uploads/dummy/test.jpg'}, format: :js
-      }.to change(AttachedAsset, :count).by(1)
-      expect(response).to be_success
-      expect(response).to render_template('documents/create')
-      expect(response).to render_template('brand_ambassadors/documents/_document')
-      expect(response).to render_template('documents/_document')
-      document = AttachedAsset.last
-      expect(document.attachable).to eq(company)
-      expect(document.asset_type).to eq('ba_document')
-      expect(document.direct_upload_url).to eq('https://s3.amazonaws.com/brandscopic-dev/uploads/dummy/test.jpg')
-      expect(AssetsUploadWorker).to have_queued(document.id, 'BrandAmbassadors::Document')
-    end
+    describe "with valid data" do
+      before do
+        ResqueSpec.reset!
+        s3object = double()
+        allow(s3object).to receive(:copy_from).and_return(true)
+        expect_any_instance_of(AWS::S3).to receive(:buckets).at_least(:once).and_return(
+          "brandscopic-dev" => double(objects: {
+            'uploads/dummy/test.jpg' => double(head: double(content_length: 100, content_type: 'image/jpeg', last_modified: Time.now)),
+            'attached_assets/original/test.jpg' => s3object
+          } ))
+        expect_any_instance_of(Paperclip::Attachment).to receive(:path).and_return('/attached_assets/original/test.jpg')
+        expect_any_instance_of(AttachedAsset).to receive(:download_url).at_least(:once).and_return('dummy.jpg')
+      end
 
-    it "should successfully create the new record for a visit" do
-      ResqueSpec.reset!
-      s3object = double()
-      allow(s3object).to receive(:copy_from).and_return(true)
-      expect_any_instance_of(AWS::S3).to receive(:buckets).at_least(:once).and_return(
-        "brandscopic-dev" => double(objects: {
-          'uploads/dummy/test.jpg' => double(head: double(content_length: 100, content_type: 'image/jpeg', last_modified: Time.now)),
-          'attached_assets/original/test.jpg' => s3object
-        } ))
-      expect_any_instance_of(Paperclip::Attachment).to receive(:path).and_return('/attached_assets/original/test.jpg')
-      expect_any_instance_of(AttachedAsset).to receive(:download_url).at_least(:once).and_return('dummy.jpg')
+      it "should successfully create the new record" do
+        expect {
+          xhr :post, 'create', brand_ambassadors_document: {direct_upload_url: 'https://s3.amazonaws.com/brandscopic-dev/uploads/dummy/test.jpg'}, format: :js
+        }.to change(AttachedAsset, :count).by(1)
+        expect(response).to be_success
+        expect(response).to render_template('documents/create')
+        expect(response).to render_template('brand_ambassadors/documents/_document')
+        expect(response).to render_template('documents/_document')
+        document = AttachedAsset.last
+        expect(document.attachable).to eq(company)
+        expect(document.asset_type).to eq('ba_document')
+        expect(document.direct_upload_url).to eq('https://s3.amazonaws.com/brandscopic-dev/uploads/dummy/test.jpg')
+        expect(document.folder_id).to be_nil
+        expect(AssetsUploadWorker).to have_queued(document.id, 'BrandAmbassadors::Document')
+      end
 
-      visit = FactoryGirl.create(:brand_ambassadors_visit,
-        company: company, company_user: user)
-      expect {
-        xhr :post, 'create', visit_id: visit.id, brand_ambassadors_document: {direct_upload_url: 'https://s3.amazonaws.com/brandscopic-dev/uploads/dummy/test.jpg'}, format: :js
-      }.to change(AttachedAsset, :count).by(1)
-      expect(response).to be_success
-      expect(response).to render_template('documents/create')
-      expect(response).to render_template('brand_ambassadors/documents/_document')
-      expect(response).to render_template('documents/_document')
-      document = AttachedAsset.last
-      expect(document.attachable).to eq(visit)
-      expect(document.asset_type).to eq('ba_document')
-      expect(document.direct_upload_url).to eq('https://s3.amazonaws.com/brandscopic-dev/uploads/dummy/test.jpg')
-      expect(AssetsUploadWorker).to have_queued(document.id, 'BrandAmbassadors::Document')
+      it "should successfully create the new record inside a folder" do
+        expect {
+          xhr :post, 'create', brand_ambassadors_document: {direct_upload_url: 'https://s3.amazonaws.com/brandscopic-dev/uploads/dummy/test.jpg'}, folder_id: 2, format: :js
+        }.to change(AttachedAsset, :count).by(1)
+        expect(response).to be_success
+        expect(response).to render_template('documents/create')
+        expect(response).to render_template('brand_ambassadors/documents/_document')
+        expect(response).to render_template('documents/_document')
+        document = AttachedAsset.last
+        expect(document.attachable).to eq(company)
+        expect(document.asset_type).to eq('ba_document')
+        expect(document.direct_upload_url).to eq('https://s3.amazonaws.com/brandscopic-dev/uploads/dummy/test.jpg')
+        expect(document.folder_id).to eql 2
+        expect(AssetsUploadWorker).to have_queued(document.id, 'BrandAmbassadors::Document')
+      end
+
+      it "should successfully create the new record for a visit" do
+        visit = FactoryGirl.create(:brand_ambassadors_visit,
+          company: company, company_user: user)
+        expect {
+          xhr :post, 'create', visit_id: visit.id, brand_ambassadors_document: {direct_upload_url: 'https://s3.amazonaws.com/brandscopic-dev/uploads/dummy/test.jpg'}, format: :js
+        }.to change(AttachedAsset, :count).by(1)
+        expect(response).to be_success
+        expect(response).to render_template('documents/create')
+        expect(response).to render_template('brand_ambassadors/documents/_document')
+        expect(response).to render_template('documents/_document')
+        document = AttachedAsset.last
+        expect(document.attachable).to eq(visit)
+        expect(document.asset_type).to eq('ba_document')
+        expect(document.direct_upload_url).to eq('https://s3.amazonaws.com/brandscopic-dev/uploads/dummy/test.jpg')
+        expect(document.folder_id).to be_nil
+        expect(AssetsUploadWorker).to have_queued(document.id, 'BrandAmbassadors::Document')
+      end
     end
 
     it "should render the form_dialog template if errors" do
