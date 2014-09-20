77380f72c383b08b681627f1f89ba172315e8aa7
Guillermo Vargas <guilleva@gmail.com>
2014-09-19 18:04:01 -0600

    Fix migration so it refresh columns of mode

--- db/migrate/20140919052121_add_group_to_custom_filters.rb
+++ db/migrate/20140919052121_add_group_to_custom_filters.rb
@@ -1,6 +1,7 @@
 class AddGroupToCustomFilters < ActiveRecord::Migration
   def change
     add_column :custom_filters, :group, :string
+    CustomFilter.reset_column_information
     CustomFilter.update_all(group: 'Saved Filters')
   end
 end
