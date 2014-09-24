0052c7518113293e869934c2210b0d0506603d53
Pablo Baltodano <palinair@hotmail.com>
2014-09-23 18:46:15 -0600

    Tweaks after code review

--- app/models/event.rb
+++ app/models/event.rb
@@ -163,7 +163,7 @@ class Event < ActiveRecord::Base
   validates :company_id, presence: true, numericality: true
   validates :start_at, presence: true
   validates :end_at, presence: true, date: { on_or_after: :start_at, message: 'must be after' }
-  validate :inside_date_range, before: :create, if: :visit
+  validate :between_visit_date_range, before: [:create, :update], if: :visit
 
   DATE_FORMAT = /\A[0-1]?[0-9]\/[0-3]?[0-9]\/[0-2]0[0-9][0-9]\z/
   validates :start_date, format: { with: DATE_FORMAT, message: 'MM/DD/YYYY' }
@@ -739,7 +739,7 @@ class Event < ActiveRecord::Base
       end
     end
 
-    def inside_date_range
+    def between_visit_date_range
       return unless start_at && end_at
       visit_start_date = self.visit.start_date.to_date
       visit_end_date = self.visit.end_date.to_date
