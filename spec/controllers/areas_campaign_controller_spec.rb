036bfa4f7419addbc33f9b15dca8c40d3555d57c
Pablo Baltodano <palinair@hotmail.com>
2014-10-10 19:20:34 -0600

    Fixing test for add place

--- spec/controllers/areas_campaign_controller_spec.rb
+++ spec/controllers/areas_campaign_controller_spec.rb
@@ -8,6 +8,7 @@ describe AreasCampaignsController, type: :controller do
   let(:company) { @user.companies.first }
   let(:area) { create(:area, company: company) }
   let(:campaign) { create(:campaign, company: company) }
+  let(:place) { create(:place, name: 'Place 1') }
 
   describe "GET 'edit'" do
     before { campaign.areas << area }
@@ -21,10 +22,10 @@ describe AreasCampaignsController, type: :controller do
   describe "POST 'add_place'" do
     before { campaign.areas << area }
     it 'add the place to the inclusions list' do
-      xhr :post, 'add_place', campaign_id: campaign.id, id: area.to_param, areas_campaign: { reference: 99 }, format: :js
+      xhr :post, 'add_place', campaign_id: campaign.id, id: area.to_param, areas_campaign: { reference: place.id.to_s }, format: :js
       expect(response).to be_success
       expect(response).to render_template 'add_place'
-      expect(campaign.areas_campaigns.first.inclusions).to eql [99]
+      expect(campaign.areas_campaigns.first.inclusions).to eql [place.id]
     end
   end
 
