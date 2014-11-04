db151fb906e8ffe659a0f458eae7568b073a8753
Guillermo Vargas <guilleva@gmail.com>
2014-11-04 15:42:27 -0600

    Removing postgres_fdw from schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20141101004857) do
   enable_extension "hstore"
   enable_extension "pg_stat_statements"
   enable_extension "pg_trgm"
-  enable_extension "postgres_fdw"
   enable_extension "tablefunc"
 
   create_table "active_admin_comments", force: true do |t|
