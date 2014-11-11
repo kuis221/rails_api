6229797c9972ec6737ae714bf2308eded64950c7
Guillermo Vargas <guilleva@gmail.com>
2014-11-10 22:55:37 -0600

    Clear area/campaign cache after customizing places so changes are reflected in permissions

--- app/models/areas_campaign.rb
+++ app/models/areas_campaign.rb
@@ -15,6 +15,7 @@ class AreasCampaign < ActiveRecord::Base
 
   after_update do
     campaign.clear_locations_cache(area)
+    Rails.cache.delete(locations_cache_key)
   end
 
   attr_accessor :reference
@@ -34,12 +35,16 @@ class AreasCampaign < ActiveRecord::Base
   end
 
   def locations
-    @locations ||= Rails.cache.fetch("area_campaign_locations_#{area_id}_#{campaign.id}") do
+    @locations ||= Rails.cache.fetch(locations_cache_key) do
       Location.joins('INNER JOIN places ON places.location_id=locations.id')
       .where(places: { id: area.place_ids + inclusions, is_location: true })
       .where.not(places: { id: exclusions + [0] })
       .group('locations.id')
     end
+  end
+
+  def locations_cache_key
+    "area_campaign_locations_#{area_id}_#{campaign.id}"
   end
 
   def location_ids
