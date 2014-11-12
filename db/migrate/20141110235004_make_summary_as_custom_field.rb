16def9452d23eb4b48cad493a833f5dd809685f8
Guillermo Vargas <guilleva@gmail.com>
2014-11-11 18:18:17 -0600

    Optimization and fixes on summary migration

--- db/migrate/20141110235004_make_summary_as_custom_field.rb
+++ db/migrate/20141110235004_make_summary_as_custom_field.rb
@@ -1,14 +1,12 @@
 class MakeSummaryAsCustomField < ActiveRecord::Migration
   def change
-    ActiveRecord::Base.transaction do
-      Campaign.all.each do |campaign|
-        field = FormField::TextArea.create(fieldable: campaign, name: 'Summary', required: false, ordering: 0)
-        campaign.events.find_each do |event|
-          next if event.summary.blank?
-          result = event.results_for([field]).first
-          result.value = event.summary
-          result.save
-        end
+    Campaign.all.each do |campaign|
+      campaign.form_fields.update_all('ordering = ordering + 1')
+      field = FormField::TextArea.create(fieldable: campaign, name: 'Summary', required: false, ordering: 0)
+      campaign.events.where.not(summary: nil).where.not(summary: '').find_each do |event|
+        result = event.results_for([field]).first
+        result.value = event.summary
+        result.save
       end
     end
 
