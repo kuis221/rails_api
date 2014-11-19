7883f70abaa993c821e1e999701d3d018770f375
Guillermo Vargas <guilleva@gmail.com>
2014-11-18 19:03:35 -0600

    Fixes for event PDF export styles

--- app/helpers/exportable_form_helper.rb
+++ app/helpers/exportable_form_helper.rb
@@ -3,7 +3,11 @@ module ExportableFormHelper
     def form
       respond_to do |format|
         format.pdf do
-          render pdf: file_name, layout: 'application.pdf', disposition: 'attachment'
+          render pdf: file_name,
+                 template: 'shared/fieldable.html.slim',
+                 layout: 'application.pdf',
+                 disposition: 'attachment',
+                 show_as_html: params[:debug].present?
         end
       end
     end
