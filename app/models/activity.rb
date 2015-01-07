2a4157c79552dff81df174921b658d4dc85178f3
Guillermo Vargas <guilleva@gmail.com>
2015-01-07 12:08:58 -0600

    Merge with development

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -25,8 +25,7 @@ class Activity < ActiveRecord::Base
   validates :activity_type_id, numericality: true, presence: true,
     inclusion: { in: :valid_activity_type_ids }
 
-  validates :campaign_id, presence: true, numericality: true,
-                          if: -> (_activitable) { activitable_type == 'Acivity' }
+  validates :campaign_id, presence: true, numericality: true
   validates :activitable_id, presence: true, numericality: true
   validates :activitable_type, presence: true
   validates :company_user_id, presence: true, numericality: true
