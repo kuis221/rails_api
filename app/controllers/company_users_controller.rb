aab2bc540660d964db115cd09580d5ac8d90650d
Kenneth Monestel <kenmonci@gmail.com>
2015-03-05 18:18:21 -0600

    Cancel login as different user

--- app/controllers/company_users_controller.rb
+++ app/controllers/company_users_controller.rb
@@ -103,17 +103,19 @@ class CompanyUsersController < FilteredController
   end
 
   def select_custom_user
-    begin
-      if current_real_company_user.is_admin?
-        user = current_company.company_users.active.find(params[:user_id])
-        if params[:user_id].to_i == current_real_company_user.id
-          session[:behave_as_user_id] = nil
-        else
-          session[:behave_as_user_id] = user.user_id
+    unless params[:commit].eql?('Cancel')
+      begin
+        if current_real_company_user.is_admin?
+          user = current_company.company_users.active.find(params[:user_id])
+          if params[:user_id].to_i == current_real_company_user.id
+            session[:behave_as_user_id] = nil
+          else
+            session[:behave_as_user_id] = user.user_id
+          end
         end
+      rescue ActiveRecord::RecordNotFound
+        flash[:error] = 'You are not allowed login as this user'
       end
-    rescue ActiveRecord::RecordNotFound
-      flash[:error] = 'You are not allowed login as this user'
     end
     redirect_to root_path
   end
