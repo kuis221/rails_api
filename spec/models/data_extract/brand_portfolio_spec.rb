f7d293253fac99831418372db1199cf94d5fa679
Kenneth Monestel <kenmonci@gmail.com>
2015-03-30 17:24:29 -0600

    Update data extract Brand Portfolios

--- spec/models/data_extract/brand_portfolio_spec.rb
+++ spec/models/data_extract/brand_portfolio_spec.rb
@@ -26,15 +26,14 @@ RSpec.describe DataExtract::BrandPortfolio, type: :model do
     let(:subject) { described_class }
 
     it 'returns the correct columns' do
-      expect(subject.exportable_columns).to eql([:name, :description, :created_by_full_name, :created_at])
+      expect(subject.exportable_columns).to eql([:name, :description, :created_by, :created_at])
     end
   end
 
-  describe '#rows', search: true do
+  describe '#rows' do
     let(:company) { create(:company) }
-    let(:subject) { described_class.new(company: company) }
-    let(:user) { create(:user, company: company) }
-    let(:company_user) { user.company_users.first }
+    let(:user) { create(:company_user, company: company) }
+    let(:subject) { described_class.new(company: company, current_user: user) }
 
     it 'returns empty if no rows are found' do
       expect(subject.rows).to be_empty
@@ -45,22 +44,59 @@ RSpec.describe DataExtract::BrandPortfolio, type: :model do
         create(:brand_portfolio, name: 'A Vinos ticos',
                                  description: 'Algunos vinos de Costa Rica',
                                  active: true, company: company, 
-                                 created_by_id: company_user.id, 
+                                 created_by_id: user.id, 
                                  created_at: Time.zone.local(2013, 8, 23, 9, 15))
-        Sunspot.commit
       end
 
       it 'returns all the events in the company with all the columns' do
         expect(subject.rows).to eql [
-          ['A Vinos ticos', 'Algunos vinos de Costa Rica', 'Test User', Time.zone.local(2013, 8, 23, 9, 15)]
+          ['A Vinos ticos', 'Algunos vinos de Costa Rica', 'Test User', "08/23/2013"]
         ]
       end
 
       it 'allows to filter the results' do
+        subject.filters = { 'active_state' => ['inactive'] }
+        expect(subject.rows).to be_empty
 
-        subject.filters = { name: ['A Vinos ticos'] }
+        subject.filters = { 'active_state' => ['active'] }
         expect(subject.rows).to eql [
-          ['A Vinos ticos', 'Algunos vinos de Costa Rica', 'Test User', Time.zone.local(2013, 8, 23, 9, 15)]
+          ['A Vinos ticos', 'Algunos vinos de Costa Rica', 'Test User', "08/23/2013"]
+        ]
+      end
+
+      it 'allows to sort the results' do
+        create(:brand_portfolio, name: 'Cervezas Ticas',
+                                 description: 'Cervezas de Costa Rica',
+                                 active: true, company: company, 
+                                 created_by_id: user.id, 
+                                 created_at: Time.zone.local(2014, 3, 26, 9, 15))
+        subject.columns = ['name', 'description', 'created_at']
+        subject.default_sort_by = 'name'
+        subject.default_sort_dir = 'ASC'
+        expect(subject.rows).to eql [
+          ["A Vinos ticos", "Algunos vinos de Costa Rica", "08/23/2013"], 
+          ["Cervezas Ticas", "Cervezas de Costa Rica", "03/26/2014"]
+        ]
+
+        subject.default_sort_by = 'name'
+        subject.default_sort_dir = 'DESC'
+        expect(subject.rows).to eql [
+          ["Cervezas Ticas", "Cervezas de Costa Rica", "03/26/2014"], 
+          ["A Vinos ticos", "Algunos vinos de Costa Rica", "08/23/2013"]
+        ]
+
+        subject.default_sort_by = 'created_at'
+        subject.default_sort_dir = 'ASC'
+        expect(subject.rows).to eql [
+          ["Cervezas Ticas", "Cervezas de Costa Rica", "03/26/2014"],
+          ["A Vinos ticos", "Algunos vinos de Costa Rica", "08/23/2013"]
+        ]
+
+        subject.default_sort_by = 'created_at'
+        subject.default_sort_dir = 'DESC'
+        expect(subject.rows).to eql [
+          ["A Vinos ticos", "Algunos vinos de Costa Rica", "08/23/2013"],
+          ["Cervezas Ticas", "Cervezas de Costa Rica", "03/26/2014"] 
         ]
       end
     end
