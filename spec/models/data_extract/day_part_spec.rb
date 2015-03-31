595fe844d3d5c20b413ae13e49fe54ddbe169f0c
Kenneth Monestel <kenmonci@gmail.com>
2015-03-31 11:39:28 -0600

    Update data extract Day Part

--- spec/models/data_extract/day_part_spec.rb
+++ spec/models/data_extract/day_part_spec.rb
@@ -27,15 +27,14 @@ RSpec.describe DataExtract::DayPart, type: :model do
 
     it 'returns the correct columns' do
       expect(subject.exportable_columns).to eql(
-       [:name, :description, :created_by_full_name, :created_at])
+       [:name, :description, :created_by, :created_at])
     end
   end
 
-  describe '#rows', search: true do
+  describe '#rows' do
     let(:company) { create(:company) }
-    let(:subject) { described_class.new(company: company) }
-    let(:user) { create(:user, company: company) }
-    let(:company_user) { user.company_users.first }
+    let(:user) { create(:company_user, company: company) }
+    let(:subject) { described_class.new(company: company, current_user: user) }
 
     it 'returns empty if no rows are found' do
       expect(subject.rows).to be_empty
@@ -43,21 +42,55 @@ RSpec.describe DataExtract::DayPart, type: :model do
 
     describe 'with data' do
       before do
-        create(:day_part, name: "Day Part 1", active: true, created_by_id: company_user.id, company: company, created_at: Time.zone.local(2013, 8, 23, 9, 15))
-        Sunspot.commit
+        create(:day_part, name: "Day Part 1", active: true, created_by_id: user.id, company: company, created_at: Time.zone.local(2013, 8, 23, 9, 15))
       end
 
       it 'returns all the events in the company with all the columns' do
         expect(subject.rows).to eql [
-          ["Day Part 1", "Some Day Part description", "Test User", Time.zone.local(2013, 8, 23, 9, 15)]
+          ["Day Part 1", "Some Day Part description", "Test User", "08/23/2013"]
         ]
       end
 
       it 'allows to filter the results' do
+        subject.filters = { 'active_state' => ['inactive'] }
+        expect(subject.rows).to be_empty
 
-        subject.filters = { name: ['Day Parte 1'] }
+        subject.filters = { 'active_state' => ['active'] }
         expect(subject.rows).to eql [
-          ["Day Part 1", "Some Day Part description", "Test User", Time.zone.local(2013, 8, 23, 9, 15)]
+          ["Day Part 1", "Some Day Part description", "Test User", "08/23/2013"]
+        ]
+      end
+
+      it 'allows to sort the results' do
+        create(:day_part, name: "Fecha 2", active: true, created_by_id: user.id, company: company, created_at: Time.zone.local(2015, 2, 12, 9, 15))
+        
+        subject.columns = ['name', 'created_at']
+        subject.default_sort_by = 'name'
+        subject.default_sort_dir = 'ASC'
+        expect(subject.rows).to eql [
+          ["Day Part 1", "08/23/2013"], 
+          ["Fecha 2", "02/12/2015"]
+        ]
+
+        subject.default_sort_by = 'name'
+        subject.default_sort_dir = 'DESC'
+        expect(subject.rows).to eql [
+          ["Fecha 2", "02/12/2015"], 
+          ["Day Part 1", "08/23/2013"]
+        ]
+
+        subject.default_sort_by = 'created_at'
+        subject.default_sort_dir = 'ASC'
+        expect(subject.rows).to eql [
+          ["Fecha 2", "02/12/2015"], 
+          ["Day Part 1", "08/23/2013"]
+        ]
+
+        subject.default_sort_by = 'created_at'
+        subject.default_sort_dir = 'DESC'
+        expect(subject.rows).to eql [
+          ["Day Part 1", "08/23/2013"], 
+          ["Fecha 2", "02/12/2015"]
         ]
       end
     end
