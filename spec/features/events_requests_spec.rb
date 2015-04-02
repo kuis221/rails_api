2cb050fb4f76fa4508f8d50d3e0244559a780742
Guillermo Vargas <guilleva@gmail.com>
2015-04-01 21:12:11 -0600

    Export event data and activities as CSV

--- spec/features/events_requests_spec.rb
+++ spec/features/events_requests_spec.rb
@@ -365,16 +365,17 @@ feature 'Events section' do
               expect(ListExportWorker).to have_queued(ListExport.last.id)
               ResqueSpec.perform_all(:export)
             end
+
             ensure_modal_was_closed
             expect(ListExport.last).to have_rows([
               ['CAMPAIGN NAME', 'AREA', 'START', 'END', 'DURATION', 'VENUE NAME', 'ADDRESS', 'CITY',
                'STATE', 'ZIP', 'ACTIVE STATE', 'EVENT STATUS', 'TEAM MEMBERS', 'CONTACTS', 'URL'],
               ['Another Campaign April 03', nil, "#{year_number}-#{month_number}-#{today.strftime('%d')}T08:00",
-               "#{year_number}-#{month_number}-#{today.strftime('%d')}T09:00", '1.0', 'Place 2',
+               "#{year_number}-#{month_number}-#{today.strftime('%d')}T09:00", '1.00', 'Place 2',
                'Place 2, 11 Main St., Los Angeles, CA, 67890', 'Los Angeles', 'CA', '67890', 'Active', 'Unsent',
                nil, nil, "http://#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}/events/#{event2.id}"],
               ['Campaign FY2012', nil, "#{year_number}-#{month_number}-#{today.strftime('%d')}T10:00",
-               "#{year_number}-#{month_number}-#{today.strftime('%d')}T11:00", '1.0', 'Place 1',
+               "#{year_number}-#{month_number}-#{today.strftime('%d')}T11:00", '1.00', 'Place 1',
                'Place 1, 11 Main St., New York City, NY, 12345', 'New York City', 'NY', '12345', 'Active',
                'Unsent', nil, 'Chris Jaskot, Guillermo Vargas',
                "http://#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}/events/#{event1.id}"]
