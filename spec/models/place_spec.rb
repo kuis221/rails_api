85cc394aa788d4cdc3325ca092d67b1f249138d3
Guillermo Vargas <guilleva@gmail.com>
2015-04-02 08:53:57 -0600

    Recover from errors when fetching results from Google's API

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -386,116 +386,29 @@ describe Place, type: :model do
     let(:google_results) { { results: [] } }
     let(:company_user) { create(:company_user, role: create(:non_admin_role)) }
 
-    before do
-      expect(described_class).to receive(:open).and_return(
-        double(read: JSON.generate(google_results))
-      )
-    end
+    describe 'without stubbed google results' do
+      it 'rescue from errors when fetching results form Google' do
+        expect(described_class).to receive(:open) { raise OpenURI::HTTPError.new('', double(:io)) }
 
-    it 'should return empty if no results' do
-      expect(described_class.combined_search(q: 'aa')).to eql []
-    end
-
-    it 'should only places valid for the current user' do
-      venue = create(:venue,
-                     place: create(:place, name: 'Qwerty', city: 'AB', state: 'California', country: 'CR'),
-                     company: company_user.company)
-      create(:venue,
-             place: create(:place, name: 'Qwerty', city: 'XY', state: 'California', country: 'CR'),
-             company: company_user.company)
-
-      Sunspot.commit
-
-      company_user.places << create(:city, name: 'AB', state: 'California', country: 'CR')
-
-      params = { q: 'qw', current_company_user: company_user }
-      expect(described_class.combined_search(params)).to eql [
-        {
-          value: 'Qwerty, 123 My Street',
-          label: 'Qwerty, 123 My Street',
-          id: venue.place_id,
-          valid: true
-        }
-      ]
-    end
-
-    describe 'with results form Google API' do
-      let(:google_results) do
-        { results: [{
-          'formatted_address' => 'Los Angeles, CA, USA',
-          'place_id' => 'PLACEID1',
-          'name' => 'Los Angeles',
-          'reference' => 'REFERENCE1',
-          'types' => %w(locality political)
-        },
-                    {
-                      'formatted_address' => 'Los Angeles, ON, Canada',
-                      'place_id' => 'PLACEID2',
-                      'name' => 'Los Angeles',
-                      'reference' => 'REFERENCE2',
-                      'types' => %w(locality political)
-                    }, {
-                      'formatted_address' => 'Tower 42, Los Angeles, CA 23211, United States',
-                      'place_id' => 'PLACEID3',
-                      'name' => 'Vertigo 42',
-                      'reference' => 'REFERENCE3',
-                      'types' => %w(food bar establishment)
-                    }] }
-      end
-
-      it "should include all the places returned by google with the 'valid' flag set to false" do
         params = { q: 'qw', current_company_user: company_user }
-        expect(described_class.combined_search(params)).to eql [
-          {
-            value: 'Los Angeles, CA, USA',
-            label: 'Los Angeles, CA, USA',
-            id: 'REFERENCE1||PLACEID1',
-            valid: false
-          },
-          {
-            value: 'Los Angeles, ON, Canada',
-            label: 'Los Angeles, ON, Canada',
-            id: 'REFERENCE2||PLACEID2',
-            valid: false
-          },
-          {
-            value: 'Vertigo 42, Tower 42, Los Angeles, CA 23211, United States',
-            label: 'Vertigo 42, Tower 42, Los Angeles, CA 23211, United States',
-            id: 'REFERENCE3||PLACEID3',
-            valid: false
-          }
-        ]
+        expect(described_class.combined_search(params)).to be_empty
+      end
+    end
+
+    describe 'with stubbed google results' do
+      before do
+        expect(described_class).to receive(:open).and_return(
+          double(read: JSON.generate(google_results))
+        )
       end
 
-      it "should set the 'valid' flag to tru for places the user is allowed to access" do
-        company_user.places << create(:city, name: 'Los Angeles', state: 'California', country: 'US')
-        params = { q: 'qw', current_company_user: company_user }
-        expect(described_class.combined_search(params)).to eql [
-          {
-            value: 'Los Angeles, CA, USA',
-            label: 'Los Angeles, CA, USA',
-            id: 'REFERENCE1||PLACEID1',
-            valid: true
-          },
-          {
-            value: 'Vertigo 42, Tower 42, Los Angeles, CA 23211, United States',
-            label: 'Vertigo 42, Tower 42, Los Angeles, CA 23211, United States',
-            id: 'REFERENCE3||PLACEID3',
-            valid: true
-          },
-          {
-            value: 'Los Angeles, ON, Canada',
-            label: 'Los Angeles, ON, Canada',
-            id: 'REFERENCE2||PLACEID2',
-            valid: false
-          }
-        ]
+      it 'should return empty if no results' do
+        expect(described_class.combined_search(q: 'aa')).to eql []
       end
 
-      it "returns mixed places from google and the app listing app's places first " do
+      it 'should only places valid for the current user' do
         venue = create(:venue,
-                       place: create(:place, name: 'Qwerty', city: 'Los Angeles',
-                                             state: 'California', country: 'US'),
+                       place: create(:place, name: 'Qwerty', city: 'AB', state: 'California', country: 'CR'),
                        company: company_user.company)
         create(:venue,
                place: create(:place, name: 'Qwerty', city: 'XY', state: 'California', country: 'CR'),
@@ -503,35 +416,133 @@ describe Place, type: :model do
 
         Sunspot.commit
 
-        company_user.places << create(:city, name: 'Los Angeles', state: 'California', country: 'US')
+        company_user.places << create(:city, name: 'AB', state: 'California', country: 'CR')
 
-        params = { q: 'Angeles', current_company_user: company_user }
+        params = { q: 'qw', current_company_user: company_user }
         expect(described_class.combined_search(params)).to eql [
           {
             value: 'Qwerty, 123 My Street',
             label: 'Qwerty, 123 My Street',
             id: venue.place_id,
             valid: true
-          },
-          {
-            value: 'Los Angeles, CA, USA',
-            label: 'Los Angeles, CA, USA',
-            id: 'REFERENCE1||PLACEID1',
-            valid: true
-          },
-          {
-            value: 'Vertigo 42, Tower 42, Los Angeles, CA 23211, United States',
-            label: 'Vertigo 42, Tower 42, Los Angeles, CA 23211, United States',
-            id: 'REFERENCE3||PLACEID3',
-            valid: true
-          },
-          {
-            value: 'Los Angeles, ON, Canada',
-            label: 'Los Angeles, ON, Canada',
-            id: 'REFERENCE2||PLACEID2',
-            valid: false
           }
         ]
+      end
+
+      describe 'with results form Google API' do
+        let(:google_results) do
+          { results: [{
+            'formatted_address' => 'Los Angeles, CA, USA',
+            'place_id' => 'PLACEID1',
+            'name' => 'Los Angeles',
+            'reference' => 'REFERENCE1',
+            'types' => %w(locality political)
+          },
+                      {
+                        'formatted_address' => 'Los Angeles, ON, Canada',
+                        'place_id' => 'PLACEID2',
+                        'name' => 'Los Angeles',
+                        'reference' => 'REFERENCE2',
+                        'types' => %w(locality political)
+                      }, {
+                        'formatted_address' => 'Tower 42, Los Angeles, CA 23211, United States',
+                        'place_id' => 'PLACEID3',
+                        'name' => 'Vertigo 42',
+                        'reference' => 'REFERENCE3',
+                        'types' => %w(food bar establishment)
+                      }] }
+        end
+
+        it "should include all the places returned by google with the 'valid' flag set to false" do
+          params = { q: 'qw', current_company_user: company_user }
+          expect(described_class.combined_search(params)).to eql [
+            {
+              value: 'Los Angeles, CA, USA',
+              label: 'Los Angeles, CA, USA',
+              id: 'REFERENCE1||PLACEID1',
+              valid: false
+            },
+            {
+              value: 'Los Angeles, ON, Canada',
+              label: 'Los Angeles, ON, Canada',
+              id: 'REFERENCE2||PLACEID2',
+              valid: false
+            },
+            {
+              value: 'Vertigo 42, Tower 42, Los Angeles, CA 23211, United States',
+              label: 'Vertigo 42, Tower 42, Los Angeles, CA 23211, United States',
+              id: 'REFERENCE3||PLACEID3',
+              valid: false
+            }
+          ]
+        end
+
+        it "should set the 'valid' flag to tru for places the user is allowed to access" do
+          company_user.places << create(:city, name: 'Los Angeles', state: 'California', country: 'US')
+          params = { q: 'qw', current_company_user: company_user }
+          expect(described_class.combined_search(params)).to eql [
+            {
+              value: 'Los Angeles, CA, USA',
+              label: 'Los Angeles, CA, USA',
+              id: 'REFERENCE1||PLACEID1',
+              valid: true
+            },
+            {
+              value: 'Vertigo 42, Tower 42, Los Angeles, CA 23211, United States',
+              label: 'Vertigo 42, Tower 42, Los Angeles, CA 23211, United States',
+              id: 'REFERENCE3||PLACEID3',
+              valid: true
+            },
+            {
+              value: 'Los Angeles, ON, Canada',
+              label: 'Los Angeles, ON, Canada',
+              id: 'REFERENCE2||PLACEID2',
+              valid: false
+            }
+          ]
+        end
+
+        it "returns mixed places from google and the app listing app's places first " do
+          venue = create(:venue,
+                         place: create(:place, name: 'Qwerty', city: 'Los Angeles',
+                                               state: 'California', country: 'US'),
+                         company: company_user.company)
+          create(:venue,
+                 place: create(:place, name: 'Qwerty', city: 'XY', state: 'California', country: 'CR'),
+                 company: company_user.company)
+
+          Sunspot.commit
+
+          company_user.places << create(:city, name: 'Los Angeles', state: 'California', country: 'US')
+
+          params = { q: 'Angeles', current_company_user: company_user }
+          expect(described_class.combined_search(params)).to eql [
+            {
+              value: 'Qwerty, 123 My Street',
+              label: 'Qwerty, 123 My Street',
+              id: venue.place_id,
+              valid: true
+            },
+            {
+              value: 'Los Angeles, CA, USA',
+              label: 'Los Angeles, CA, USA',
+              id: 'REFERENCE1||PLACEID1',
+              valid: true
+            },
+            {
+              value: 'Vertigo 42, Tower 42, Los Angeles, CA 23211, United States',
+              label: 'Vertigo 42, Tower 42, Los Angeles, CA 23211, United States',
+              id: 'REFERENCE3||PLACEID3',
+              valid: true
+            },
+            {
+              value: 'Los Angeles, ON, Canada',
+              label: 'Los Angeles, ON, Canada',
+              id: 'REFERENCE2||PLACEID2',
+              valid: false
+            }
+          ]
+        end
       end
     end
   end
