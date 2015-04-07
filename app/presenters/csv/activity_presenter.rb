4b6f352c680c675e28715f1a28aeeb579ff843c7
Guillermo Vargas <guilleva@gmail.com>
2015-04-07 12:59:25 -0600

    Formatting dates and td linx code in csv file

--- app/presenters/csv/activity_presenter.rb
+++ app/presenters/csv/activity_presenter.rb
@@ -1,11 +1,15 @@
 module Csv
   class ActivityPresenter < BasePresenter
     def date
-      Timeliness.parse(@model.activity_date.strftime('%Y-%m-%d %H:%M:%S'), zone: 'UTC').strftime('%FT%R')
+      Timeliness.parse(@model.activity_date.strftime('%Y-%m-%d %H:%M:%S'), zone: 'UTC').strftime('%F %R')
     end
 
     def place_address
       h.strip_tags(h.event_place_address(@model, false, ', ', ', '))
+    end
+
+    def place_td_linx_code
+      "=\"#{@model.place_td_linx_code}\"" unless @model.place_td_linx_code.blank?
     end
   end
 end
