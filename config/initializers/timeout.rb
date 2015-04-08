e89e558cb8c07fe2fbb2933c23effd93cbaf6262
Guillermo Vargas <guilleva@gmail.com>
2015-04-08 11:23:25 -0600

    Fixing tests and changing how activities are displayed

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,1 +1,5 @@
-Rack::Timeout.timeout = 25  # seconds
+if Rails.env.test?
+  Rack::Timeout.timeout = 0  # disabled in test to avoid capybara intermittent timeouts
+else
+  Rack::Timeout.timeout = 25  # seconds
+end
