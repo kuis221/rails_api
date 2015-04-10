463cb9a9f07de6b853d653f98fefaa569d0a0636
Kenneth Monestel <kenmonci@gmail.com>
2015-04-09 21:43:42 -0600

    Update Campaigns

--- app/models/data_extract/campaign.rb
+++ app/models/data_extract/campaign.rb
@@ -22,8 +22,12 @@
 class DataExtract::Campaign < DataExtract
   define_columns name: 'campaigns.name', 
                  description: 'campaigns.description', 
-                 brands_list: 'array_to_string(array_agg(brands.name), \', \')', 
-                 campaign_brand_portfolios: 'array_to_string(array_agg(brand_portfolios.name), \', \')',
+                 brands_list: 'array_to_string(ARRAY(SELECT brands.name FROM brands 
+                              LEFT JOIN brands_campaigns ON campaigns.id=brands_campaigns.campaign_id 
+                              WHERE brands.id=brands_campaigns.brand_id ),\', \') AS brands_list',
+                 campaign_brand_portfolios: 'array_to_string(ARRAY(SELECT brand_portfolios.name FROM brand_portfolios 
+                              LEFT JOIN brand_portfolios_campaigns ON campaigns.id=brand_portfolios_campaigns.campaign_id 
+                              WHERE brand_portfolios.id=brand_portfolios_campaigns.brand_portfolio_id ),\', \') AS campaign_brand_portfolios',
                  start_date: proc { "to_char(campaigns.start_date, 'MM/DD/YYYY')" }, 
                  end_date: proc { "to_char(campaigns.end_date, 'MM/DD/YYYY')" }, 
                  color: 'color', 
@@ -33,23 +37,13 @@ class DataExtract::Campaign < DataExtract
   def add_joins_to_scope(s)
     s = s.joins('LEFT JOIN places ON places.id=events.place_id') if columns.any? { |c| c.match(/^place_/)  }
     if columns.include?('created_by') || filters.present? && filters['user'].present?
-      s = s.joins('LEFT JOIN users ON brands.created_by_id=users.id')
+      s = s.joins('LEFT JOIN users ON campaigns.created_by_id=users.id')
     end
-    if columns.include?('brands_list')
-      s = s.joins('LEFT JOIN brands_campaigns ON campaigns.id=brands_campaigns.campaign_id')
-           .joins('LEFT JOIN brands ON brands.id=brands_campaigns.brand_id')
-    end
-    if columns.include?('campaign_brand_portfolios')
-      s = s.joins('LEFT JOIN brand_portfolios_campaigns ON campaigns.id=brand_portfolios_campaigns.campaign_id')
-           .joins('LEFT JOIN brand_portfolios ON brand_portfolios.id=brand_portfolios_campaigns.brand_portfolio_id')
-    end
-    s.group('campaigns.id')
   end
 
-  def group_by_columns(column_name)
-    (
-      ['campaigns.id'] + columns.each_with_index.map { |c, i| i + 1 } -
-      [columns.index(column_name) + 1]
-    ).join(',')
+  def add_filter_conditions_to_scope(s)
+    return s if filters.nil? || filters.empty?
+    s = s.where(aasm_state: filters['status'].map { |f| f.downcase == 'active' ? 'active' : 'inactive' }) if filters['status'].present?
+    s
   end
 end
--- spec/models/data_extract/campaign_spec.rb
+++ spec/models/data_extract/campaign_spec.rb
@@ -22,21 +22,21 @@
 require 'rails_helper'
 
 RSpec.describe DataExtract::Campaign, type: :model do
-  pending '#available_columns' do
+  describe '#available_columns' do
     let(:subject) { described_class }
 
     it 'returns the correct columns' do
       expect(subject.exportable_columns).to eql(
         [:name, :description, :brands_list, :campaign_brand_portfolios,
-        :start_date, :end_date, :color, :created_by_full_name, :created_at])
+         :start_date, :end_date, :color, :created_by, :created_at])
     end
   end
 
-  pending '#rows', search: true do
+  describe '#rows' do
     let(:company) { create(:company) }
-    let(:subject) { described_class.new(company: company) }
     let(:company_user) { create(:company_user, company: company,
                          user: create(:user, first_name: 'Benito', last_name: 'Camelas')) }
+    let(:subject) { described_class.new(company: company, current_user: company_user) }
 
     it 'returns empty if no rows are found' do
       expect(subject.rows).to be_empty
@@ -45,21 +45,22 @@ RSpec.describe DataExtract::Campaign, type: :model do
     describe 'with data' do
       before do
         create(:campaign, name: 'Campaign Absolut FY12', description: 'Description campaign', company: company,
-                          created_by_id: user.id, color: '#de4d43', created_at: Time.zone.local(2013, 8, 23, 9, 15))
-        Sunspot.commit
+                          created_by_id: company_user.user.id, color: '#de4d43', created_at: Time.zone.local(2013, 8, 23, 9, 15))
       end
 
       it 'returns all the events in the company with all the columns' do
         expect(subject.rows).to eql [
-          ["Campaign Absolut FY12", "Description campaign", "", "", nil, nil, "#de4d43", "Test User", Time.zone.local(2013, 8, 23, 9, 15)]
+          ["Campaign Absolut FY12", "Description campaign", "", "", nil, nil, "#de4d43", "Benito Camelas", "08/23/2013"]
         ]
       end
 
       it 'allows to filter the results' do
+        subject.filters = { 'status' => ['inactive'] }
+        expect(subject.rows).to be_empty
 
-        subject.filters = { color: ['#de4d43'] }
+        subject.filters = { 'status' => ['active'] }
         expect(subject.rows).to eql [
-          ["Campaign Absolut FY12", "Description campaign", "", "", nil, nil, "#de4d43", "Test User", Time.zone.local(2013, 8, 23, 9, 15)]
+          ["Campaign Absolut FY12", "Description campaign", "", "", nil, nil, "#de4d43", "Benito Camelas", "08/23/2013"]
         ]
       end
     end
