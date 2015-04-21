e7a96906061763c44bb0fe3593315b3aad6a9815
Guillermo Vargas <guilleva@gmail.com>
2015-04-21 17:09:11 -0600

    Updating schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -65,12 +65,10 @@ ActiveRecord::Schema.define(version: 20150418003746) do
   create_table "activity_types", force: true do |t|
     t.string   "name"
     t.text     "description"
-    t.boolean  "active",        default: true
+    t.boolean  "active",      default: true
     t.integer  "company_id"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-    t.integer  "created_by_id"
-    t.integer  "updated_by_id"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   add_index "activity_types", ["company_id"], :name => "index_activity_types_on_company_id"
@@ -344,10 +342,8 @@ ActiveRecord::Schema.define(version: 20150418003746) do
     t.string   "state"
     t.string   "city"
     t.string   "zip_code"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.integer  "created_by_id"
-    t.integer  "updated_by_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "custom_filters", force: true do |t|
@@ -370,28 +366,6 @@ ActiveRecord::Schema.define(version: 20150418003746) do
   end
 
   add_index "custom_filters_categories", ["company_id"], :name => "index_custom_filters_categories_on_company_id"
-
-  create_table "data_extracts", force: true do |t|
-    t.string   "type"
-    t.integer  "company_id"
-    t.boolean  "active"
-    t.string   "sharing"
-    t.string   "name"
-    t.text     "description"
-    t.text     "filters"
-    t.text     "columns"
-    t.integer  "created_by_id"
-    t.integer  "updated_by_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "default_sort_by"
-    t.string   "default_sort_dir"
-    t.text     "params"
-  end
-
-  add_index "data_extracts", ["company_id"], :name => "index_data_extracts_on_company_id"
-  add_index "data_extracts", ["created_by_id"], :name => "index_data_extracts_on_created_by_id"
-  add_index "data_extracts", ["updated_by_id"], :name => "index_data_extracts_on_updated_by_id"
 
   create_table "data_migrations", force: true do |t|
     t.integer  "remote_id"
@@ -791,7 +765,7 @@ ActiveRecord::Schema.define(version: 20150418003746) do
     t.string   "name"
     t.string   "reference",              limit: 400
     t.string   "place_id",               limit: 100
-    t.string   "types_old"
+    t.string   "types"
     t.string   "formatted_address"
     t.string   "street_number"
     t.string   "route"
@@ -812,7 +786,6 @@ ActiveRecord::Schema.define(version: 20150418003746) do
     t.spatial  "lonlat",                 limit: {:srid=>4326, :type=>"point", :geographic=>true}
     t.integer  "td_linx_confidence"
     t.integer  "merged_with_place_id"
-    t.string   "types",                                                                                        array: true
   end
 
   add_index "places", ["city"], :name => "index_places_on_city"
@@ -854,14 +827,12 @@ ActiveRecord::Schema.define(version: 20150418003746) do
 
   create_table "roles", force: true do |t|
     t.string   "name"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.integer  "company_id"
-    t.boolean  "active",        default: true
+    t.boolean  "active",      default: true
     t.text     "description"
-    t.boolean  "is_admin",      default: false
-    t.integer  "created_by_id"
-    t.integer  "updated_by_id"
+    t.boolean  "is_admin",    default: false
   end
 
   create_table "satisfaction_surveys", force: true do |t|
@@ -1039,15 +1010,10 @@ ActiveRecord::Schema.define(version: 20150418003746) do
     t.boolean  "score_dirty",                                   default: false
     t.boolean  "jameson_locals",                                default: false
     t.boolean  "top_venue",                                     default: false
-    t.integer  "created_by_id"
-    t.integer  "updated_by_id"
   end
 
   add_index "venues", ["company_id", "place_id"], :name => "index_venues_on_company_id_and_place_id", :unique => true
   add_index "venues", ["company_id"], :name => "index_venues_on_company_id"
   add_index "venues", ["place_id"], :name => "index_venues_on_place_id"
-
-  create_table "views_for_data_extracts", force: true do |t|
-  end
 
 end
