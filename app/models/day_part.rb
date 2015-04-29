1f32e8adae4dcdb1df6edbcb86eb775a283a8cf2
Guillermo Vargas <guilleva@gmail.com>
2015-04-29 17:12:06 -0600

    Fixes on module blank states

--- app/models/day_part.rb
+++ app/models/day_part.rb
@@ -25,6 +25,8 @@ class DayPart < ActiveRecord::Base
 
   has_many :day_items
 
+  scope :active, -> { where(active: true) }
+
   searchable do
     integer :id
 
@@ -57,7 +59,6 @@ class DayPart < ActiveRecord::Base
         with(:company_id, params[:company_id])
         with(:status, params[:status]) if params.key?(:status) && params[:status].present?
         with(:id, params[:day_part]) if params.key?(:day_part) && params[:day_part].present?
-
 
         facet :status if include_facets
 
