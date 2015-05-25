519f4c7d3000277715502569556c81e86ccd9508
Guillermo Vargas <guilleva@gmail.com>
2015-05-25 15:40:24 -0600

    Adding tasks to remove stale/zombie workers

--- app/workers/venue_indexer.rb
+++ app/workers/venue_indexer.rb
@@ -6,6 +6,9 @@ class VenueIndexer
     Rails.logger.info "Started VenueIndexer job: venue_id=#{venue_id}"
     Venue.find(venue_id).compute_stats
     Rails.logger.info "Finished VenueIndexer job: venue_id=#{venue_id}"
-  rescue
+  rescue Resque::TermException, Resque::DirtyExit
+    # if the worker gets killed, (when deploying for example)
+    # re-enqueue the job so it will be processed when worker is restarted
+    Resque.enqueue(VenueIndexer, venue_id)
   end
 end
