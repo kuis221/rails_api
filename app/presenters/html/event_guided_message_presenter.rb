f049625407f5a2be490ae969c7c8ba626189fd85
Kenneth Monestel <kenmonci@gmail.com>
2015-05-27 16:45:34 -0600

    instructional messages to be relevant to the event phase

--- app/presenters/html/event_guided_message_presenter.rb
+++ app/presenters/html/event_guided_message_presenter.rb
@@ -74,6 +74,7 @@ module Html
     end
 
     def results_approve_per
+      return complete_message_step(:approve_per) unless is_current_phase
       if can?(:approve)
         rejection_message = if @model.reject_reason.to_s.present?
           "It was previously rejected #{rejected_at} for the following reason: <i>#{@model.reject_reason}.</i> "
@@ -88,11 +89,13 @@ module Html
 
     def results_last
       return '' unless @model.approved?
+      return complete_message_step(:last) unless is_current_phase
       message_with_buttons 'Your post event report has been approved. Check out your post event results below for a recap of your event.', :last,
                            [unapprove_button]
     end
 
     def yes_or_skip_or_back(message, step)
+      return complete_message_step(step) unless is_current_phase
       target = "#event-#{step}"
       next_target = next_target_after(step)
       prev_target = prev_target_before(step)
@@ -108,6 +111,7 @@ module Html
     end
 
     def info(message, step)
+      return complete_message_step(step) unless is_current_phase
       prev_target = prev_target_before(step)
       [
         h.link_to('', "#event-#{step}", data: { spytarget: "#event-#{step}" }),
@@ -123,6 +127,14 @@ module Html
        ] + [h.content_tag(:div, buttons.compact.join.html_safe, class: 'step-buttons')]).join.html_safe
     end
 
+    def complete_message_step(step)
+      message = h.present(@model).current_phase == :plan ? "You've completed the planning phase of your event." : "You've completed the execute phase of your event."
+      [
+        h.link_to('', "#event-#{step}", data: { spytarget: "#event-#{step}" }),
+        message,
+      ].join.html_safe
+    end
+      
     def next_target_after(step)
       index = current_steps.index { |s| s[:id] == step }
       next_step = current_steps[index + 1] || nil
@@ -136,26 +148,26 @@ module Html
     end
 
     def unapprove_button
-      return unless can?(:unapprove)
+      return unless can?(:unapprove) && is_current_phase
       h.button_to 'Unapprove', h.unapprove_event_path(@model, return: h.return_path),
                   method: :put, class: 'btn btn-cancel'
     end
 
     def approve_button
-      return unless can?(:approve)
+      return unless can?(:approve) && is_current_phase
       h.button_to 'Approve', h.approve_event_path(@model, return: h.return_path),
                   method: :put, class: 'btn btn-primary'
     end
 
     def reject_button
-      return unless can?(:reject)
+      return unless can?(:reject) && is_current_phase
       h.button_to 'Reject', h.reject_event_path(@model, format: :js, return: h.return_path),
                   form: { id: 'reject-post-event' },
                   method: :put, class: 'btn btn-cancel', remote: true
     end
 
     def submit_button
-      return unless can?(:submit)
+      return unless can?(:submit) && is_current_phase
       h.button_to 'Submit', h.submit_event_path(@model, format: :js, return: h.return_path),
                   class: 'btn btn-cancel', method: :put,
                   remote: true, data: { disable_with: 'submitting' }
@@ -179,5 +191,8 @@ module Html
       end
     end
 
+    def is_current_phase
+      h.present(@model).current_phase == phases[:current_phase]
+    end
   end
 end
