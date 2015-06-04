8242f6ca2b71ce8d242a630bea7a01e529e3200d
Guillermo Vargas <guilleva@gmail.com>
2015-06-04 10:53:12 -0600

    Revert "Removing not used test file"
    This reverts commit 63ef107788bcf86d5f029ad4d0692071d3d3500e.

---
+++ spec/factories/event_expenses.rb
@@ -0,0 +1,31 @@
+# == Schema Information
+#
+# Table name: event_expenses
+#
+#  id            :integer          not null, primary key
+#  event_id      :integer
+#  amount        :decimal(15, 2)   default(0.0)
+#  created_by_id :integer
+#  updated_by_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  brand_id      :integer
+#  category      :string(255)
+#  expense_date  :date
+#  reimbursable  :boolean
+#  billable      :boolean
+#  merchant      :string(255)
+#  description   :text
+#
+
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :event_expense do
+    event nil
+    amount '9.99'
+    category 'Entertainment'
+    expense_date '01/01/2015'
+    brand_id nil
+  end
+end
