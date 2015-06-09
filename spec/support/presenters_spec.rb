51bf376b0ac3f3aa1d6c0bc80d63c83996b43ee5
Guillermo Vargas <guilleva@gmail.com>
2015-06-09 11:07:43 -0600

    Adding support for presenters in Rspec

---
+++ spec/support/presenters_spec.rb
@@ -0,0 +1,25 @@
+module Brandscopic
+  module SimpleDelegator
+    module RSpec
+      @@view_context = nil
+      def self.enable(example)
+        example.extend self
+
+        controller = Class.new(ApplicationController).new
+        controller.request = ActionController::TestRequest.new
+        @@view_context = controller.view_context
+        self
+      end
+
+      def present(obj)
+        @@view_context.present(obj)
+      end
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.before :each, type: :presenter do
+    Brandscopic::SimpleDelegator::RSpec.enable(self)
+  end
+end
