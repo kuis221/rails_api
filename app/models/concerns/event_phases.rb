0290bd4e0b07282c2396c9d315203c62883028f1
Kenneth Monestel <kenmonci@gmail.com>
2015-06-18 17:59:30 -0600

    Notice that there is no Next Action

--- app/models/concerns/event_phases.rb
+++ app/models/concerns/event_phases.rb
@@ -37,7 +37,7 @@ module EventPhases
     phase = execute_phases.dup
     if current_phase == :execute
       phase.push(id: :per, title: 'Submit',
-                  complete: false, required: false) if phase.find{ |p| p[:complete] == false }.nil?
+                  complete: false, required: false) if phase.all?{ |p| p[:complete] }
     end
     phase
   end
