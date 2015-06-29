ff4aa3c789e78f444faf2f5ab010a6b223813ed5
Guillermo Vargas <guilleva@gmail.com>
2015-06-29 08:17:31 -0600

    Fixing PERs export

---
+++ app/assets/javascripts/pdf.js
@@ -0,0 +1,12 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+// 
+// This file is used to indicate what JS files are required
+// for generating somm PDF properly
+// 
+//= require jquery
+//= require jquery-ui-1.10.4.custom
+//= require bootstrap
+//= require jquery.photoGallery
+//= require jquery.uniform
+//= require highstock.src.js
