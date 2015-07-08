fb1e40de7761c3aef9b7670281ffa6a912c8bb60
Łukasz Śliwa <lukasz.sliwa@apptamers.com>
2015-07-08 16:28:27 +0200

    [#97778906] Exporting comments to CSV with Country column.

--- app/controllers/results/comments_controller.rb
+++ app/controllers/results/comments_controller.rb
@@ -1,10 +1,21 @@
 class Results::CommentsController < FilteredController
   defaults resource_class: ::Event
-  respond_to :xls, only: :index
+  respond_to :csv, only: :index
 
   helper_method :return_path
 
   private
+
+  def collection_to_csv
+    CSV.generate do |csv|
+      csv << ['CAMPAIGN NAME', 'VENUE NAME', 'ADDRESS', 'COUNTRY', 'EVENT START DATE', 'EVENT END DATE', 'COMMENT']
+      each_collection_item do |event|
+        event.comments.each do |comment|
+          csv << [event.campaign_name, event.place_name, event.place_address, event.place_country, event.start_date, event.end_date, comment.content]
+        end
+      end
+    end
+  end
 
   def search_params
     @search_params || (super.tap do |p|
