5b4ebe30140d7a3bec9781b602f3607cf647c349
Kenneth Monestel <kenmonci@gmail.com>
2015-07-16 19:34:38 -0600

    Add templates for the report

--- app/controllers/analysis/campaign_summary_report_controller.rb
+++ app/controllers/analysis/campaign_summary_report_controller.rb
@@ -1,6 +1,13 @@
 class Analysis::CampaignSummaryReportController < ApplicationController
 
+  respond_to :xls, :pdf, only: :index
+
   helper_method :return_path
+
+  def report
+    @campaign ||= current_company.campaigns.find(params[:report][:campaign_id]) if params[:report] && params[:report][:campaign_id].present?
+    render layout: false
+  end
 
   protected
 
