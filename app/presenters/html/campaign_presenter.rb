e55d9605371476bb9d434827622d5eeaf4f24aeb
Kenneth Monestel <kenmonci@gmail.com>
2015-07-20 18:47:10 -0600

    Create form field age

--- app/presenters/html/campaign_presenter.rb
+++ app/presenters/html/campaign_presenter.rb
@@ -124,13 +124,37 @@ module Html
       total = total_male + total_female
 
       {
-        male: percent_of(total_male, total),
-        female: percent_of(total_female, total)
+        male: percent_of(total_male, total).round,
+        female: percent_of(total_female, total).round
       }
     end
 
     def percent_of(n, t)
       n.to_f / t.to_f * 100.0
+    end
+
+    def result_for_age(form_field)
+      totals = initialize_totals(form_field)
+
+      events.active.each do |event|
+        result = event.results_for([form_field]).first
+        if result.hash_value.present? || result.value.present?
+          form_field.options_for_input.each do |_, id|
+            if form_field.type_name == 'Radio'
+              totals[result.value.to_i] += 1 if id == result.value.to_i
+            else
+              totals[id] += result.hash_value[id.to_s].to_f if result.hash_value[id.to_s].present?
+            end
+          end
+        end
+      end
+
+      total = totals.inject(0) {|total, (_, value)| total += value }
+      options_map = Hash[form_field.options_for_input.map{|o| [o[1], o[0]] }]
+      totals.inject({}) do |memo, (key, value)|
+        memo[options_map[key]] = percent_of(value, total).round(2)
+        memo
+      end
     end
   end
 end
