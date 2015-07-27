ca10b8a5ac649f8639d22e9065cde4bdbad1dbd5
Pablo Baltodano <palinair@hotmail.com>
2015-07-27 13:50:29 -0600

    [Finished #97139674] Story: As a Legacy JBB user I want to better understand why the RSVP synch has failed

--- app/mailers/rsvp_report_mailer.rb
+++ app/mailers/rsvp_report_mailer.rb
@@ -6,9 +6,10 @@ class RsvpReportMailer < ActionMailer::Base
     mail to: recipients, subject: 'RSVP Report Synch – File Not Found'
   end
 
-  def invalid_format(files, columns)
+  def invalid_format(files, columns, invalid_columns)
     recipients = ENV['RSVP_INVALID_FORMAT_EMAILS'].split(',')
     @columns = columns
+    @invalid_columns = invalid_columns
     files.each do |path|
       attachments[File.basename(path)] = File.read(path)
     end
@@ -26,5 +27,17 @@ class RsvpReportMailer < ActionMailer::Base
     end if files
 
     mail to: recipients, subject: 'RSVP Report Synch – Successfully Completed'
+  end
+
+  def fail(failed, error_messages, files = [])
+    @failed = failed
+    @error_messages = error_messages
+    recipients = ENV['RSVP_SUCCESS_EMAILS'].split(',')
+
+    files.each do |path|
+      attachments[File.basename(path)] = File.read(path)
+    end if files
+
+    mail to: recipients, subject: 'RSVP Report Synch – Failed'
   end
 end
