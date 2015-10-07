c17011fb29354f3e836317a80ddf2b89f0fda4ab
Guillermo Vargas <guilleva@gmail.com>
2015-10-07 07:54:31 -0600

    Fixing code after rubocop autofixes

--- spec/models/data_extract/event_spec.rb
+++ spec/models/data_extract/event_spec.rb
@@ -47,7 +47,9 @@ RSpec.describe DataExtract::Event, type: :model do
     let(:campaign) { create(:campaign, name: 'Campaign Absolut FY12', company: company) }
     let(:subject) do
       described_class.new(company: company, current_user: company_user,
-                    columns: %w(campa          ign_name end_date end_time start_date start_time place_street place_city place_name place_state           place_zipcode event_team_members event_status created_by created_at status))
+                    columns: %w(campaign_name end_date end_time start_date start_time place_street
+                                place_city place_name place_state place_zipcode event_team_members
+                                event_status created_by created_at status))
     end
 
     it 'returns empty if no rows are found' do
