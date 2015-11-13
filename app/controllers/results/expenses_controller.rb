d649ad946467cf3419eca12fa7a930fff835dd46
Kenneth Monestel <kenmonci@gmail.com>
2015-11-13 15:30:30 -0600

    104322642 generate file zip

--- app/controllers/results/expenses_controller.rb
+++ app/controllers/results/expenses_controller.rb
@@ -1,12 +1,26 @@
 class Results::ExpensesController < FilteredController
   defaults resource_class: ::Event
   respond_to :csv, only: :index
+  respond_to :zip, only: :index
 
   helper_method :expenses_total, :return_path
 
   private
 
   def collection_to_csv
+    export_collection
+  end
+
+  def collection_to_zip(export, path)
+    Zip::File.open(path, Zip::File::CREATE) do |zipfile|
+      csv = export_collection do |event|
+        add_expenses_files_zip(event, zipfile)
+      end
+      add_expenses_csv_file(csv, zipfile)
+    end
+  end
+
+  def export_collection(&block)
     exporter = EventExpensesExporter.new(current_company_user, search_params)
     CSV.generate do |csv|
       csv << [
@@ -18,8 +32,45 @@ class Results::ExpensesController < FilteredController
           event.campaign_name, event.place_name, event.place_address, event.country, event.start_date, event.end_date,
           event.first_event_expense_created_at, event.first_event_expense_created_by, event.last_event_expense_updated_at, event.last_event_expense_updated_by
         ].concat(exporter.event_expenses(event))
+        yield event if block_given?
       end
     end
+  end
+
+  def export_list(export, path)
+    @_export = export
+    if export.export_format == 'zip'
+      run_callbacks :export do
+        collection_to_zip(export, path)
+      end
+    else
+      super
+    end
+  end
+
+  def add_expenses_files_zip(event, zipfile)
+    event.event_expenses.each_with_index do |expense, index|
+      file_local_name = "#{Rails.root}/tmp/#{expense.id}"
+      expense.receipt.file.copy_to_local_file(:medium, file_local_name) if expense.receipt.present?
+      zipfile.add generate_filename(event, expense, index), file_local_name if expense.receipt.present?
+    end
+  end
+
+  def add_expenses_csv_file(csv, zipfile)
+    csv_file = Tempfile.new('expenses')
+    csv_file.write csv
+    csv_file.close
+    zipfile.add "expenses-#{Time.now.strftime('%Y%m%d%H%M%S')}.csv", csv_file.path
+  end
+
+  def generate_filename(event, expense, index)
+    user = User.find(expense.created_by_id) if expense.created_by_id.present?
+    created_name = user.present? ? "#{user.first_name[0]}{user.last_name}": ''
+    "#{expense.expense_date.strftime('%Y%m%d')}-#{remove_all_spaces(event.place_name)}-#{remove_all_spaces(expense.category)}-#{created_name}-#{index}.#{expense.receipt.file_extension}"
+  end
+
+  def remove_all_spaces(value)
+    value.gsub(/\s+/, '')
   end
 
   def search_params
