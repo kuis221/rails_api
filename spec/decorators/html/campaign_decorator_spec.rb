77c7371002fef38ba5f0c328b36ec94ce6495dc8
Kenneth Monestel <kenmonci@gmail.com>
2015-11-13 13:15:55 -0600

    Remove presenters repeated

--- spec/decorators/html/campaign_decorator_spec.rb
+++ spec/decorators/html/campaign_decorator_spec.rb
@@ -7,13 +7,10 @@ describe Html::CampaignPresenter, type: :presenter do
       presenter = present(build(:campaign))
       expect(presenter.gender_percentage([])).to eq({ male: 0, female: 0 })
 
-      presenter = present(build(:campaign))
       expect(presenter.gender_percentage([30,40])).to eq({ male: 0, female: 0 })
 
-      presenter = present(build(:campaign))
       expect(presenter.gender_percentage([['Male'],['Female', 40.0]])).to eq({ male: 0, female: 100 })
 
-      presenter = present(build(:campaign))
       expect(presenter.gender_percentage([['Female', 230.0], ['Male', 270.0]])).to eq({ male: 54, female: 46 })
     end
   end
