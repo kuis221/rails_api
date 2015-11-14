819ff00d446dd8b54ed0de9ae8c4bbf792628cb9
Kenneth Monestel <kenmonci@gmail.com>
2015-11-14 15:16:41 -0600

    Add test for Zip::EventPresenter and Creator for event expenses

--- app/presenters/zip/event_presenter.rb
+++ app/presenters/zip/event_presenter.rb
@@ -1,18 +1,19 @@
 module Zip
   class EventPresenter < Csv::EventPresenter
     def receipts_for_zip_export
-      @model.event_expenses.each_with_index.inject([]) do |m, (expense, index)|
-        file_local_name = "#{Rails.root}/tmp/#{expense.id}"
-        resolution = expense.receipt.is_thumbnable? ? :medium : :original
-        expense.receipt.file.copy_to_local_file(resolution, file_local_name) if expense.receipt.present?
-        m << [generate_filename(expense, index), file_local_name] if expense.receipt.present?
+      @model.event_expenses.includes(:creator).each_with_index.inject([]) do |m, (expense, index)|
+        if expense.receipt.present?
+          file_local_name = "#{Rails.root}/tmp/#{expense.id}"
+          resolution = expense.receipt.is_thumbnable? ? :medium : :original
+          expense.receipt.file.copy_to_local_file(resolution, file_local_name)
+          m << [generate_filename(expense, index), file_local_name]
+        end
         m
       end
     end
 
     def generate_filename(expense, index)
-      user = User.find(expense.created_by_id) if expense.created_by_id.present?
-      created_name = user.present? ? "#{user.first_name[0]}#{user.last_name}": ''
+      created_name = expense.creator.present? ? "#{expense.creator.first_name[0]}#{expense.creator.last_name}": ''
       expense_date = expense.expense_date.strftime('%Y%m%d')
       venue_name = remove_all_spaces(@model.place_name)
       category = remove_all_spaces(expense.category)
