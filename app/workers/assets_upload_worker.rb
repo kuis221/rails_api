9dc60316b40ee1e1ab607618d1ebe1610a59f7cb
Guillermo Vargas <guilleva@gmail.com>
2015-11-15 21:37:59 -0600

    Do not process processed assets

--- app/workers/assets_upload_worker.rb
+++ app/workers/assets_upload_worker.rb
@@ -5,6 +5,7 @@ class AssetsUploadWorker
   def perform(asset_id, asset_class = 'AttachedAsset')
     klass ||= asset_class.constantize
     asset = klass.find(asset_id)
+    return if asset.processed?
     asset.queued! unless asset.queued?
     asset.transfer_and_cleanup
   end
