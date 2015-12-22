cbb3503a1f9f03915e55b4303e4227da52143650
Jeff Chen <chien-hung.chen@case.edu>
2015-12-22 17:02:24 -0500

    missed a piece when manually merging

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20151202183554) do
+ActiveRecord::Schema.define(version: 20151217231146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
